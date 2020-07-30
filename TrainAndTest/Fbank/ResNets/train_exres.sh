8fe1f7c61cbda9f1f59a4847fc88c3ab8ef82423
MacWilliam <874681044@qq.com>
2020-07-30 15:10:50 +0800

    timittimit

--- TrainAndTest/Fbank/ResNets/train_exres.sh
+++ TrainAndTest/Fbank/ResNets/train_exres.sh
@@ -16,9 +16,9 @@ if [ $stage -le 0 ]; then
   datasets=vox1
   feat=fb64_3w
   loss=soft
-  for encod in SAP SASP STAP None ; do
+  for encod in None ; do
     echo -e "\n\033[1;4;31m Training ${model}_${encod} with ${loss}\033[0m\n"
-    python -W ignore TrainAndTest/Fbank/ResNets/train_exres_var.py \
+    python -W ignore TrainAndTest/Fbank/ResNets/train_exres_kaldi.py \
       --train-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/pydb/dev_${feat} \
       --test-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/pydb/test_${feat} \
       --nj 8 \
@@ -39,8 +39,8 @@ if [ $stage -le 0 ]; then
       --test-input-per-file 1 \
       --lr 0.1 \
       --encoder-type ${encod} \
-      --check-path Data/checkpoint/${model}10/${datasets}_${encod}_rmavg/${feat}/${loss}_var \
-      --resume Data/checkpoint/${model}10/${datasets}_${encod}_rmavg/${feat}/${loss}_var/checkpoint_100.pth \
+      --check-path Data/checkpoint/${model}10/${datasets}_${encod}_avg/${feat}/${loss} \
+      --resume Data/checkpoint/${model}10/${datasets}_${encod}_avg/${feat}/${loss}/checkpoint_100.pth \
       --input-per-spks 384 \
       --veri-pairs 9600 \
       --gpu-id 0 \
