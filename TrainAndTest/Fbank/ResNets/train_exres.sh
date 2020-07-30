600e825b79233c585deaebfc7a03756243b0cd35
MacWilliam <874681044@qq.com>
2020-07-30 21:29:01 +0800

    fast

--- TrainAndTest/Fbank/ResNets/train_exres.sh
+++ TrainAndTest/Fbank/ResNets/train_exres.sh
@@ -16,7 +16,7 @@ if [ $stage -le 0 ]; then
   datasets=vox1
   feat=fb64_3w
   loss=soft
-  for encod in SAP SASP STAP None ; do
+  for encod in None ; do
     echo -e "\n\033[1;4;31m Training ${model}_${encod} with ${loss}\033[0m\n"
     python -W ignore TrainAndTest/Fbank/ResNets/train_exres_kaldi.py \
       --train-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/pydb/dev_${feat} \
@@ -32,6 +32,7 @@ if [ $stage -le 0 ]; then
       --batch-size 128 \
       --feat-dim 64 \
       --remove-vad \
+      --fast \
       --time-dim 8 \
       --avg-size 1 \
       --kernel-size 5,5 \
@@ -39,8 +40,8 @@ if [ $stage -le 0 ]; then
       --test-input-per-file 1 \
       --lr 0.1 \
       --encoder-type ${encod} \
-      --check-path Data/checkpoint/${model}10/${datasets}_${encod}_avg/${feat}/${loss} \
-      --resume Data/checkpoint/${model}10/${datasets}_${encod}_avg/${feat}/${loss}/checkpoint_100.pth \
+      --check-path Data/checkpoint/${model}10/${datasets}_${encod}_fast/${feat}/${loss} \
+      --resume Data/checkpoint/${model}10/${datasets}_${encod}_fast/${feat}/${loss}/checkpoint_100.pth \
       --input-per-spks 384 \
       --veri-pairs 9600 \
       --gpu-id 0 \
