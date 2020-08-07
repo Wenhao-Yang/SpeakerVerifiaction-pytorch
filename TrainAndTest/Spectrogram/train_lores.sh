d63747f7bd698e32341052ae49800047e9bf45ef
MacWilliam <874681044@qq.com>
2020-08-07 17:34:31 +0800

    voxceleb 1 finetune

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -267,14 +267,14 @@ if [ $stage -le 31 ]; then
       --input-per-spks 224 \
       --feat-format kaldi \
       --nj 8 \
-      --epochs 22 \
+      --epochs 12 \
       --resnet-size ${resnet_size} \
       --embedding-size 128 \
       --avg-size 4 \
       --alpha 12 \
       --inst-norm \
       --batch-size 128 \
-      --milestones 8,13,18 \
+      --milestones 6,10 \
       --channels 64,128,256 \
       --check-path Data/checkpoint/GradResNet8_inst/${dataset}_power/spect/${loss}_dp25 \
       --resume Data/checkpoint/GradResNet8_inst/${dataset}_power/spect/soft_dp25/checkpoint_24.pth \
