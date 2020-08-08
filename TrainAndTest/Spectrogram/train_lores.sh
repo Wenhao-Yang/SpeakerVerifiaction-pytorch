b5ab302eb4b71e73bb1ca754a76d98281bcbbfba
MacWilliam <874681044@qq.com>
2020-08-08 19:02:38 +0800

    voxceleb 1 finetune

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -258,7 +258,7 @@ if [ $stage -le 31 ]; then
   dataset=vox1
   model=GradResNet
   resnet_size=8
-  for loss in asoft center ; do # 32,128,512; 8,32,128
+  for loss in center ; do # 32,128,512; 8,32,128
     echo -e "\n\033[1;4;31m Training with ${loss} kernel 5,5\033[0m\n"
     python -W ignore TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --model ${model} \
@@ -276,10 +276,11 @@ if [ $stage -le 31 ]; then
       --batch-size 128 \
       --milestones 6,10 \
       --channels 64,128,256 \
-      --check-path Data/checkpoint/GradResNet8_inst/${dataset}_power/spect/${loss}_dp25 \
+      --check-path Data/checkpoint/GradResNet8_inst/${dataset}_power/spect/${loss}_dp25_l2 \
       --resume Data/checkpoint/GradResNet8_inst/${dataset}_power/spect/soft_dp25/checkpoint_24.pth \
       --finetune \
       --loss-type ${loss} \
+      --loss-ratio 0.5 \
       --lr 0.01 \
       --num-valid 2 \
       --gpu-id 0 \
