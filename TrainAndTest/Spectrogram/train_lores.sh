4b0bd39fe3d37fc0d853c43f854ac77a4dc8545a
MacWilliam <874681044@qq.com>
2020-08-16 10:09:03 +0800

    rm mean for vox1 asoft am cne

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -258,7 +258,7 @@ if [ $stage -le 31 ]; then
   dataset=vox1
   model=GradResNet
   resnet_size=8
-  for loss in soft ; do # 32,128,512; 8,32,128
+  for loss in asoft amsoft center; do # 32,128,512; 8,32,128
     echo -e "\n\033[1;4;31m Training with ${loss} kernel 5,5\033[0m\n"
     python -W ignore TrainAndTest/Spectrogram/train_lores10_kaldi.py \
       --model ${model} \
@@ -267,19 +267,22 @@ if [ $stage -le 31 ]; then
       --input-per-spks 224 \
       --feat-format kaldi \
       --nj 10 \
-      --epochs 24 \
+      --epochs 15 \
       --resnet-size ${resnet_size} \
       --embedding-size 128 \
       --avg-size 4 \
       --alpha 12 \
       --inst-norm \
       --batch-size 128 \
-      --milestones 10,15,20 \
+      --milestones 4,8,12 \
       --channels 64,128,256 \
       --check-path Data/checkpoint/GradResNet8_mean/${dataset}_power/spect_time/${loss}_dp25 \
-      --resume Data/checkpoint/GradResNet8_mean/${dataset}_power/spect_time/soft_dp25/checkpoint_18.pth \
+      --resume Data/checkpoint/GradResNet8_mean/${dataset}_power/spect_time/soft_dp25/checkpoint_24.pth \
       --loss-type ${loss} \
-      --loss-ratio 0.5 \
+      --finetune \
+      --margin 0.3 \
+      --s 30 \
+      --loss-ratio 0.1 \
       --lr 0.1 \
       --num-valid 2 \
       --gpu-id 0 \
