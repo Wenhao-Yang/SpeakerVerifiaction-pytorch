b0374beb065497ddb15676c27c4cebe011eb30d6
MacWilliam <874681044@qq.com>
2020-08-21 12:31:21 +0800

    vox1 lmdb train 32 job args

--- Process_Data/Compute_Feat/make_egs.py
+++ Process_Data/Compute_Feat/make_egs.py
@@ -210,6 +210,14 @@ if __name__ == "__main__":
 
     pool = Pool(processes=nj)  # 创建nj个进程
     for i in range(0, nj):
+        pool.apply_async(PrepareEgProcess, args=(lock_i, lock_t, train_dir, task_queue, error_queue))
+
+    pool.close()  # 关闭进程池，表示不能在往进程池中添加进程
+    pool.join()  # 等待进程池中的所有进程执行完毕，必须在close()之后调用
+
+
+    pool = Pool(processes=nj)  # 创建nj个进程
+    for i in range(0, nj):
         write_dir = os.path.join(out_dir, 'Split%d/%d' % (nj, i))
         if not os.path.exists(write_dir):
             os.makedirs(write_dir)
@@ -218,10 +226,8 @@ if __name__ == "__main__":
         if not os.path.exists(ark_dir):
             os.makedirs(ark_dir)
 
-        pool.apply_async(PrepareEgProcess, args=(lock_i, lock_t, train_dir, task_queue, error_queue))
-        if i + 1 % 2 == 1:
-            pool.apply_async(SaveEgProcess, args=(lock_t, write_dir, ark_dir, args.out_set,
-                                                  i, task_queue, error_queue, idx_queue))
+        pool.apply_async(SaveEgProcess, args=(lock_t, write_dir, ark_dir, args.out_set,
+                                              i, task_queue, error_queue, idx_queue))
 
     pool.close()  # 关闭进程池，表示不能在往进程池中添加进程
     pool.join()  # 等待进程池中的所有进程执行完毕，必须在close()之后调用
