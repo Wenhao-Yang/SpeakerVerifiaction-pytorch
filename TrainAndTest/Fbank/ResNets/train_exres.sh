b92e56a0c55022bfb1542907e8219f3d606d3789
MacWilliam <874681044@qq.com>
2020-08-25 18:09:35 +0800

    257 egs valid ex

--- TrainAndTest/Fbank/ResNets/train_exres.sh
+++ TrainAndTest/Fbank/ResNets/train_exres.sh
@@ -218,8 +218,8 @@ if [ $stage -le 20 ]; then
       --lr 0.1 \
       --loss-ratio 0.1 \
       --encoder-type ${encod} \
-      --check-path Data/checkpoint/${model}34_filter/${datasets}_${encod}/${feat}/${loss}_mean \
-      --resume Data/checkpoint/${model}34_filter/${datasets}_${encod}/${feat}/${loss}_mean/checkpoint_100.pth \
+      --check-path Data/checkpoint/${model}34_filter/${datasets}_${encod}/${feat}/${loss}_mean_0.5 \
+      --resume Data/checkpoint/${model}34_filter/${datasets}_${encod}/${feat}/${loss}_mean_0.5/checkpoint_100.pth \
       --input-per-spks 384 \
       --cos-sim \
       --veri-pairs 9600 \
