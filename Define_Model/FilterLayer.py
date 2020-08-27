b40d478647cee6a04c547f946b9f052a0944b232
MacWilliam <874681044@qq.com>
2020-08-27 15:11:46 +0800

    grl layer

--- Define_Model/FilterLayer.py
+++ Define_Model/FilterLayer.py
@@ -52,3 +52,31 @@ class fDLR(nn.Module):
         weights = weights.mul(self.gain).transpose(0, 1)
 
         return torch.matmul(input, weights)
+
+
+class grl_func(torch.autograd.Function):
+    def __init__(self):
+        super(grl_func, self).__init__()
+
+    @staticmethod
+    def forward(ctx, x, lambda_):
+        ctx.save_for_backward(lambda_)
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        lambda_, = ctx.saved_variables
+        grad_input = grad_output.clone()
+        return - lambda_ * grad_input, None
+
+
+class GRL(nn.Module):
+    def __init__(self, lambda_=0.):
+        super(GRL, self).__init__()
+        self.lambda_ = torch.tensor(lambda_)
+
+    def set_lambda(self, lambda_):
+        self.lambda_ = torch.tensor(lambda_)
+
+    def forward(self, x):
+        return grl_func.apply(x, self.lambda_)
