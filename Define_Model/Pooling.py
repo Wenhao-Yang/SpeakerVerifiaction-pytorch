f5398aa898defd115e84d63c469eabe74b31bc82
MacWilliam <874681044@qq.com>
2020-09-03 11:46:38 +0800

    egs for cnceleb inst vad

--- Define_Model/Pooling.py
+++ Define_Model/Pooling.py
@@ -15,23 +15,32 @@ import torch.nn as nn
 
 
 class SelfVadPooling(nn.Module):
-    def __init__(self, input_dim):
+    def __init__(self, input_dim, input_length=300):
         super(SelfVadPooling, self).__init__()
-        self.conv = nn.Conv1d(1, 1, kernel_size=5, stride=1, padding=2)
+        self.conv1 = nn.Conv1d(1, 1, kernel_size=5, stride=1, padding=2)
         self.fc1 = nn.Linear(input_dim, 1)
-        self.bn = nn.BatchNorm1d(1)
+        self.bn1 = nn.BatchNorm1d(1)
+
+        self.conv2 = nn.Conv1d(1, 1, kernel_size=5, stride=1, padding=2)
+        self.fc2 = nn.Linear(input_length, 1)
+        self.bn2 = nn.BatchNorm1d(1)
+
         self.activation = nn.Hardtanh(min_val=0.001, max_val=1.0)
         # nn.init.constant(self.fc1.weight, 0.1)
 
     def forward(self, x):
         x_energy = self.fc1(x).squeeze(-1)  # .log()
-        x_energy = self.bn(x_energy)
-        # x_energy = torch.sum(x, dim=-1)  # .log()
-        vad = self.conv(x_energy).unsqueeze(-1)
-        x_weight = self.activation(vad)
+        x_energy = self.bn1(x_energy)
+        vad = self.conv1(x_energy).unsqueeze(-1)
+        vad_weight = self.activation(vad)
+
+        x_freq = self.fc2(x.transpose(2, 3)).squeeze(-1)  # .log()
+        x_freq = self.bn2(x_freq)
+        freq = self.conv2(x_freq).unsqueeze(2)
+        freq_weight = self.activation(freq)
 
         # x_weight = 2. * x_weight - x_weight.pow(2)
-        return x * x_weight
+        return x * vad_weight * freq_weight
 
 
 class SelfAttentionPooling(nn.Module):
