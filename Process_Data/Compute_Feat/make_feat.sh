203fb432f8c74beca083261f5e05996837bfac9c
MacWilliam <874681044@qq.com>
2020-09-20 13:02:34 +0800

    120 egs radio v2 inst

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -664,19 +664,19 @@ if [ $stage -le 130 ]; then
 
 
   done
-  python Process_Data/Compute_Feat/make_feat.py \
-        --data-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/test_8k_radio_v3 \
-        --out-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/spect \
-        --out-set test_8k_radio_v3 \
-        --feat-type spectrogram \
-        --lowfreq 300 \
-        --highfreq 3000 \
-        --bandpass \
-        --feat-format kaldi \
-        --nfft 160 \
-        --windowsize 0.02 \
-        --log-scale \
-        --nj 18
+#  python Process_Data/Compute_Feat/make_feat.py \
+#        --data-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/test_8k_radio_v3 \
+#        --out-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/spect \
+#        --out-set test_8k_radio_v3 \
+#        --feat-type spectrogram \
+#        --lowfreq 300 \
+#        --highfreq 3000 \
+#        --bandpass \
+#        --feat-format kaldi \
+#        --nfft 160 \
+#        --windowsize 0.02 \
+#        --log-scale \
+#        --nj 18
 #  for s in dev test ; do
 #    python Process_Data/Compute_Feat/make_feat.py \
 #        --data-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/vox1/${s}_8k_wav \
