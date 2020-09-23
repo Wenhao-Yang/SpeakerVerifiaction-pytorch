38c4364381ebf513295463b1fea8f7dd9253b780
MacWilliam <874681044@qq.com>
2020-09-23 10:17:18 +0800

    inst test

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -470,10 +470,9 @@ if [ $stage -le 60 ]; then
       --extract \
       --test-input-per-file 4 \
       --inst-norm \
-      --transform \
       --stride 2 \
-      --xvector-dir Data/xvector/LoResNet${resnet_size}/army_v1/spect_egs_tran/soft_dp01_example \
-      --resume  Data/checkpoint/LoResNet${resnet_size}/army_v1/spect_egs_tran/soft_dp01/checkpoint_20.pth \
+      --xvector-dir Data/xvector/LoResNet${resnet_size}/army_v1/spect_egs_inst/soft_dp01_example \
+      --resume  Data/checkpoint/LoResNet${resnet_size}/army_v1/spect_egs_inst/soft_dp01/checkpoint_20.pth \
       --trials trials \
       --gpu-id 0
   done
