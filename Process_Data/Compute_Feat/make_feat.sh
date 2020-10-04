7c529285533274be0f4eff27ded3c73c6b27be77
MacWilliam <874681044@qq.com>
2020-10-04 22:45:00 +0800

    timit linear

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -915,9 +915,10 @@ if [ $stage -le 200 ]; then
     python Process_Data/Compute_Feat/make_feat.py \
       --data-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/${name} \
       --out-dir /home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb \
-      --out-set ${name}_var \
+      --out-set ${name}_mel \
       --feat-type fbank \
-      --filter-type dnn.timit.var \
+      --filter-type mel \
+      --log-scale \
       --nfft 320 \
       --windowsize 0.02 \
        --filters 23
