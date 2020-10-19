d5a2a33489df0d1b01c4744791f5837c7d9651be
MacWilliam <874681044@qq.com>
2020-10-19 21:52:08 +0800

    vox1 13 egs

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -346,10 +346,10 @@ if [ $stage -le 50 ]; then
       --epochs 20 \
       --lr 0.1 \
       --milestones 5,10,15 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/spect_egs_STAP/${loss}_dp25_64 \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/spect_egs_STAP/${loss}_dp25_64/checkpoint_24.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/spect_egs_STAP/${loss}_dp25_64_13 \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/spect_egs_STAP/${loss}_dp25_64_13/checkpoint_24.pth \
       --channels 64,64,128,256 \
-      --stride 2 \
+      --stride 1,3 \
       --batch-size 128 \
       --embedding-size 128 \
       --avg-size 1 \
