98ee0bda64eac9b672cb560e116b1d4bd2a30176
MacWilliam <874681044@qq.com>
2020-10-26 21:49:59 +0800

    vox2 egs

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -800,14 +800,14 @@ if [ $stage -le 63 ]; then
       --feat-format kaldi \
       --resnet-size ${resnet_size} \
       --input-norm Mean \
-      --batch-size 128 \
+      --batch-size 192 \
       --nj 12 \
       --epochs 24 \
       --lr 0.1 \
       --input-dim 81 \
       --milestones 8,14,20 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}_x/spect_egs/${loss}_dp25 \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}_x/spect_egs/soft_dp25/checkpoint_24.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}_x/spect_egs/${loss}_dp25_b192 \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}_x/spect_egs/soft_dp25_b192/checkpoint_24.pth \
       --channels 64,128,256 \
       --embedding-size 128 \
       --avg-size 4 \
