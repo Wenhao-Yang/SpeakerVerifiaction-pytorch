4c18074148bc616138256040c7b5385bbfd3c8f8
MacWilliam <874681044@qq.com>
2020-10-31 15:32:12 +0800

    vox2 egs

--- Vector_Score/enroll_eval.sh
+++ Vector_Score/enroll_eval.sh
@@ -6,7 +6,7 @@ if [ $stage -le 0 ]; then
 
   python Vector_Score/enroll_eval.py \
     --data-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/army/spect/thre_enrolled \
-    --enroll-dir Data/xvector/LoResNet10/army/spect_81/soft/x_vector \
+    --extract-path Data/xvector/LoResNet10/army/spect_81/soft/x_vector \
     --test-dir Data/xvector/LoResNet10/army/spect_81/soft/x_vector \
     --split-set
 
