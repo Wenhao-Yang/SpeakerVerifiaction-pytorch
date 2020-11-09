5dbb3f661b3d860b1b1a9419e5184943cb1d53cb
MacWilliam <874681044@qq.com>
2020-11-09 16:06:49 +0800

    army multi

--- Vector_Score/enroll_eval.sh
+++ Vector_Score/enroll_eval.sh
@@ -17,7 +17,7 @@ if [ $stage -le 10 ]; then
 
   python Vector_Score/enroll_eval.py \
     --data-dir /home/work2020/yangwenhao/project/lstm_speaker_verification/data/army/spect/thre_enrolled \
-    --extract-path Data/xvector/MultiResNet10/army/spect_81/soft/x_vector \
+    --extract-path Data/xvector/MultiResNet10/army/spect_81/soft/x_vector/enrollled \
     --test-dir Data/xvector/LoResNet10/army/spect_81/soft/x_vector \
     --split-set
 
