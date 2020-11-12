a16a2a2129728092b9aec89d056a57bd13bff5f9
MacWilliam <874681044@qq.com>
2020-11-12 22:13:10 +0800

    fast army mutlti 29epoch train

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -877,12 +877,12 @@ if [ $stage -le 64 ]; then
       --input-norm MeanStd \
       --batch-size 256 \
       --nj 10 \
-      --epochs 16 \
-      --lr 0.01 \
+      --epochs 24 \
+      --lr 0.1 \
       --input-dim 81 \
       --fast \
       --stride 1 \
-      --milestones 6,12 \
+      --milestones 8,14,20 \
       --check-path Data/checkpoint/${model}${resnet_size}/${datasets}_x2/spect_egs_${encod}/${loss}_dp25_b256_${alpha}_fast_std \
       --resume Data/checkpoint/${model}${resnet_size}/${datasets}_x2/spect_egs_${encod}/${loss}_dp25_b256_${alpha}_fast_std/checkpoint_8.pth \
       --channels 16,64,128,256 \
