5cfdaf86e80684f3a22dddcb18506e877c510581
MacWilliam <874681044@qq.com>
2020-11-25 14:21:12 +0800

    tdnn vox2

--- TrainAndTest/Spectrogram/train_egs.py
+++ TrainAndTest/Spectrogram/train_egs.py
@@ -438,6 +438,7 @@ def main():
 
     if args.cuda:
         if len(args.gpu_id) > 1:
+            torch.distributed.init_process_group(backend="nccl", rank=0)
             model = model.cuda()
             model = DistributedDataParallel(model)
 
