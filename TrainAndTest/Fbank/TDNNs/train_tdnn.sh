ce55de16762fc8f00ab3c0049329c2900b4ad28b
Mac-Yang <874681044@qq.com>
2020-12-28 19:06:40 +0800

    [Train] TDNN_v4 var length in a batch

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -325,7 +325,6 @@ if [ $stage -le 80 ]; then
       --train-trials trials_2w \
       --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/valid_${feat} \
       --test-dir ${lstm_dir}/data/vox1/${feat_type}/test_${feat} \
-      --random-chunk 250 400 \
       --nj 12 \
       --epochs 30 \
       --patience 2 \
