4aa322a1e9fb04273938365f01d5c68484a40be8
Mac-Yang <874681044@qq.com>
2020-12-28 12:27:42 +0800

    [Train] TDNN_v4 var length in a batch

--- Process_Data/audio_processing.py
+++ Process_Data/audio_processing.py
@@ -598,7 +598,7 @@ class PadCollate:
         # pad according to max_len
         # print()
         xs = torch.stack(list(map(lambda x: x[0], batch)), dim=0)
-        start = np.random.randint(low=0, high=batch[0][0].shape[-2] - frame_len + 1)
+        start = np.random.randint(low=0, high=batch[0][1].shape[-2] - frame_len + 1)
         end = start + frame_len
         xs = xs[:, :, start:end, :]
         ys = torch.LongTensor(list(map(lambda x: x[1], batch)))
