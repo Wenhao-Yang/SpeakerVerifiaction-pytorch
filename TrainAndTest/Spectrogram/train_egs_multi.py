426e5c5ff6eff89e968d56a7005bb03b6b012b59
Mac-Yang <874681044@qq.com>
2021-01-11 21:42:06 +0800

    [filter] time freq

--- TrainAndTest/Spectrogram/train_egs_multi.py
+++ TrainAndTest/Spectrogram/train_egs_multi.py
@@ -359,7 +359,8 @@ def main():
             checkpoint = torch.load(args.resume)
             start_epoch = checkpoint['epoch']
 
-            filtered = {k: v for k, v in checkpoint['state_dict'].items() if 'num_batches_tracked' not in k}
+            # filtered = {k: v for k, v in checkpoint['state_dict'].items() if 'num_batches_tracked' not in k}
+            filtered = {k: v for k, v in checkpoint['state_dict'] if 'num_batches_tracked' not in k}
             model_dict = model.state_dict()
             model_dict.update(filtered)
             model.load_state_dict(model_dict)
