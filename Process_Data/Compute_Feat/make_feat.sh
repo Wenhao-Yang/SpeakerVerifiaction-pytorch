6fd4779df93ea42c3f77e43782a6494daf8d84d0
Mac-Yang <874681044@qq.com>
2021-03-17 00:43:45 +0800

    [data] army v5 log

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=130
+stage=232
 # voxceleb1
 lstm_dir=/home/work2020/yangwenhao/project/lstm_speaker_verification
 
@@ -1235,20 +1235,20 @@ fi
 if [ $stage -le 232 ]; then
   datasets=army
 
-  #  python Process_Data/Compute_Feat/make_egs.py \
-  #    --nj 10 \
-  #    --data-dir ${lstm_dir}/data/${datasets}/spect/dev_8k_v5_log \
-  #    --out-dir ${lstm_dir}/data/${datasets}/egs/spect \
-  #    --feat-type spectrogram \
-  #    --train \
-  #    --input-per-spks 768 \
-  #    --feat-format kaldi \
-  #    --out-format kaldi_cmp \
-  #    --num-valid 2 \
-  #    --out-set dev_8k_v5_log
+  python Process_Data/Compute_Feat/make_egs.py \
+    --nj 12 \
+    --data-dir ${lstm_dir}/data/${datasets}/spect/dev_8k_v5_log \
+    --out-dir ${lstm_dir}/data/${datasets}/egs/spect \
+    --feat-type spectrogram \
+    --train \
+    --input-per-spks 768 \
+    --feat-format kaldi \
+    --out-format kaldi_cmp \
+    --num-valid 2 \
+    --out-set dev_8k_v5_log
 
   python Process_Data/Compute_Feat/make_egs.py \
-    --nj 10 \
+    --nj 12 \
     --data-dir ${lstm_dir}/data/${datasets}/spect/dev_8k_v5_log \
     --out-dir ${lstm_dir}/data/${datasets}/egs/spect \
     --feat-type spectrogram \
