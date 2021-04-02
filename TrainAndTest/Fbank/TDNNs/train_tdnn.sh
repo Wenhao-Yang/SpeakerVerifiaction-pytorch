fe11cba807956b64710b6b83995bada7d99ebed6
william-asus <874681044@qq.com>
2021-04-02 17:02:36 +0800

    [train] shuffle

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -489,7 +489,7 @@ if [ $stage -le 90 ]; then
       --block-type ${block_type} \
       --scheduler rop \
       --weight-decay 0.0001 \
-      --lr 0.01 \
+      --lr 0.1 \
       --alpha 0 \
       --feat-format kaldi \
       --embedding-size ${embedding_size} \
