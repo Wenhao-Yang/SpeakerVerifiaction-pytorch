30b4d7f00d29fe40b783515531fc00f533df11f9
Mac-Yang <874681044@qq.com>
2021-05-15 19:56:48 +0800

    [cnceleb] log spect tdnn

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=82
+stage=80
 lstm_dir=/home/work2020/yangwenhao/project/lstm_speaker_verification
 
 # ===============================    LoResNet10    ===============================
@@ -571,19 +571,23 @@ if [ $stage -le 80 ]; then
   loss=arcsoft
   encod=None
   dataset=vox1
+  test_set=cnceleb
 
-  # 20210426
+  # 20210426 vox1 test
   # Test ERR is 2.3542%, Threshold is 0.2698025107383728
   # mindcf-0.01 0.2192, mindcf-0.001 0.2854.
+
+  # test_set=cnceleb
+
   for loss in arcsoft; do # 32,128,512; 8,32,128
-    echo -e "\n\033[1;4;31m Stage ${stage}: Testing with ${loss} \033[0m\n"
+    echo -e "\n\033[1;4;31m Stage ${stage}: Testing ${test_set} with ${loss} \033[0m\n"
     python -W ignore TrainAndTest/test_egs.py \
       --model TDNN_v5 \
       --train-dir ${lstm_dir}/data/vox2/${feat_type}/dev_${feat} \
       --train-test-dir ${lstm_dir}/data/vox1/${feat_type}/dev_${feat}/trials_dir \
       --train-trials trials_2w \
       --valid-dir ${lstm_dir}/data/vox1/${feat_type}/valid_${feat} \
-      --test-dir ${lstm_dir}/data/vox1/${feat_type}/test_${feat} \
+      --test-dir ${lstm_dir}/data/${test_set}/${feat_type}/test_${feat} \
       --feat-format kaldi \
       --input-norm Mean \
       --input-dim 161 \
@@ -596,7 +600,7 @@ if [ $stage -le 80 ]; then
       --s 30 \
       --input-length var \
       --frame-shift 300 \
-      --xvector-dir Data/xvector/TDNN_v5/vox2_v2/spect_egs/arcsoft_0ce/inputMean_STAP_em512_wde4/epoch_60_var \
+      --xvector-dir Data/xvector/TDNN_v5/vox2_v2/spect_egs/arcsoft_0ce/inputMean_STAP_em512_wde4/${test_set}_epoch_60_var \
       --resume Data/checkpoint/TDNN_v5/vox2_v2/spect_egs/arcsoft_0ce/inputMean_STAP_em512_wde4/checkpoint_60.pth \
       --gpu-id 0 \
       --cos-sim
@@ -667,9 +671,11 @@ if [ $stage -le 82 ]; then
   # mindcf-0.01 0.2346, mindcf-0.001 0.4054.
 
   test_set=cnceleb
+#  Test ERR is 19.5295%, Threshold is 0.28571683168411255
+#  mindcf-0.01 0.7313, mindcf-0.001 0.8193.
 
   for loss in arcsoft; do # 32,128,512; 8,32,128
-    echo -e "\n\033[1;4;31m Testing with ${loss} \033[0m\n"
+    echo -e "\n\033[1;4;31m Stage${stage}: Testing with ${loss} \033[0m\n"
     python -W ignore TrainAndTest/test_egs.py \
       --model ${model} \
       --resnet-size 14 \
