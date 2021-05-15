b3621b96f7fcd115a2f67d03354ca3abf9f08c1b
Mac-Yang <874681044@qq.com>
2021-05-15 18:57:19 +0800

    [cnceleb] log spect

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=80
+stage=82
 lstm_dir=/home/work2020/yangwenhao/project/lstm_speaker_verification
 
 # ===============================    LoResNet10    ===============================
@@ -658,13 +658,15 @@ if [ $stage -le 82 ]; then
   # 1.6172%, Threshold is 0.29920902848243713
   # mindcf-0.01 0.1592, mindcf-0.001 0.2065.
 
-  test_set=sitw
+  # test_set=sitw
   # dev
   # Test ERR is 2.3489%, Threshold is 0.2773
   # mindcf-0.01 0.2098, mindcf-0.001 0.3596.
   # eval
   # Test ERR is 2.6791%, Threshold is 0.2732
   # mindcf-0.01 0.2346, mindcf-0.001 0.4054.
+
+  test_set=cnceleb
 
   for loss in arcsoft; do # 32,128,512; 8,32,128
     echo -e "\n\033[1;4;31m Testing with ${loss} \033[0m\n"
@@ -675,7 +677,7 @@ if [ $stage -le 82 ]; then
       --train-test-dir ${lstm_dir}/data/vox1/${feat_type}/dev_${feat}/trials_dir \
       --train-trials trials_2w \
       --valid-dir ${lstm_dir}/data/${train_set}/${feat_type}/valid_${feat} \
-      --test-dir ${lstm_dir}/data/${test_set}/${feat_type}/eval_${feat} \
+      --test-dir ${lstm_dir}/data/${test_set}/${feat_type}/test_${feat} \
       --feat-format kaldi \
       --input-norm ${input_norm} \
       --input-dim 161 \
@@ -690,7 +692,7 @@ if [ $stage -le 82 ]; then
       --loss-type ${loss} \
       --encoder-type STAP \
       --input-length var \
-      --xvector-dir Data/xvector/RET14/vox2/spect_STAP_v2/arcsoft_0ce/em512_inputMean_cbam_bs128_wde4_shuf/${test_set}_eval_epoch20_var \
+      --xvector-dir Data/xvector/RET14/vox2/spect_STAP_v2/arcsoft_0ce/em512_inputMean_cbam_bs128_wde4_shuf/${test_set}_test_epoch20_var \
       --resume Data/checkpoint/RET14/vox2/spect_STAP_v2/arcsoft_0ce/em512_inputMean_cbam_bs128_wde4_shuf/checkpoint_20.pth \
       --gpu-id 0 \
       --cos-sim
