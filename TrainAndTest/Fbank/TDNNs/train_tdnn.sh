d7a1bb65f6feaf404c7bebd83148c71364b529ef
Mac-Yang <874681044@qq.com>
2021-05-28 18:54:37 +0800

    [train] cnceleb center

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -459,7 +459,7 @@ if [ $stage -le 77 ]; then
       --channels 512,512,512,512,1500 \
       --encoder-type ${encod} \
       --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs/${loss}_lrr${lr_ratio}_lsr${loss_ratio}/feat${feat}_input${input_norm}_${encod}_em${embedding_size}_wd5e4_var \
-      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs/${loss}__lrr${lr_ratio}_lsr${loss_ratio}/feat${feat}_input${input_norm}_${encod}_em${embedding_size}_wd5e4_var/checkpoint_10.pth \
+      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs/${loss}_lrr${lr_ratio}_lsr${loss_ratio}/feat${feat}_input${input_norm}_${encod}_em${embedding_size}_wd5e4_var/checkpoint_13.pth \
       --cos-sim \
       --dropout-p 0.0 \
       --veri-pairs 9600 \
