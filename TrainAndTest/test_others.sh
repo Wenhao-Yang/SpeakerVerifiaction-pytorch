1fbc2d419c3a534b3af5002a5a498bc107b8ebc6
Mac-Yang <874681044@qq.com>
2021-05-31 16:59:01 +0800

    [extract] vox1 fb40 ws25 arcsoft

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -622,7 +622,6 @@ if [ $stage -le 76 ]; then
       --xvector-dir Data/xvector/TDNN_v5/vox1/pyfb_egs_baseline/soft/featfb40_ws25_inputMean_STAP_em256_wd5e4_var/${test_set}_${subset}_epoch_40_var \
       --resume Data/checkpoint/TDNN_v5/vox1/pyfb_egs_baseline/soft/featfb40_ws25_inputMean_STAP_em256_wd5e4_var/checkpoint_40.pth \
       --gpu-id 1 \
-      --extract \
       --remove-vad \
       --cos-sim
   done
