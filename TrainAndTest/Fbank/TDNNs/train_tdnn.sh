3d5d95bd070ef1f4e3fca8eded4bca27b59973c7
Mac-Yang <874681044@qq.com>
2021-06-16 13:20:08 +0800

    [ecapa] arcsoft

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -468,7 +468,7 @@ if [ $stage -le 78 ]; then
       --gpu-id 0,1 \
       --num-valid 2 \
       --loss-type ${loss} \
-      --margin 0.15 \
+      --margin 0.2 \
       --s 30 \
       --all-iteraion 50 \
       --remove-vad \
