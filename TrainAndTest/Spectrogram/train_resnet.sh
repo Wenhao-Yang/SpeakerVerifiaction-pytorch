f06765b640961b74af13eeb7d840426abe62006f
Mac-Yang <874681044@qq.com>
2021-07-21 12:23:15 +0800

    [train] ecapa 512 2s-3s

--- TrainAndTest/Spectrogram/train_resnet.sh
+++ TrainAndTest/Spectrogram/train_resnet.sh
@@ -108,7 +108,7 @@ if [ $stage -le 21 ]; then
   feat_type=klsp
   sname=dev
 
-  for block_type in basic seblock; do
+  for block_type in seblock; do
     echo -e "\n\033[1;4;31mStage ${stage}: Training ${model}${resnet_size} in ${datasets}_egs with ${loss} \033[0m\n"
     python TrainAndTest/Spectrogram/train_egs.py \
       --model ${model} \
@@ -128,8 +128,8 @@ if [ $stage -le 21 ]; then
       --accu-steps 1 \
       --lr 0.1 \
       --milestones 10,20,40,50 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/input${input_norm}_${encoder_type}_em${embedding_size}_alpha${alpha}_wde3_${sname}_fix \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/input${input_norm}_${encoder_type}_em${embedding_size}_alpha${alpha}_wde3_${sname}_fix/checkpoint_10.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/input${input_norm}_${block_type}_${encoder_type}_em${embedding_size}_alpha${alpha}_wde3_${sname}_fix \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/input${input_norm}_${block_type}_${encoder_type}_em${embedding_size}_alpha${alpha}_wde3_${sname}_fix/checkpoint_10.pth \
       --channels 16,32,64,128 \
       --input-dim 161 \
       --block-type ${block_type} \
