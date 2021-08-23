292a89fe4e441a4a461d5476d87293882f55a6d0
william-asus <874681044@qq.com>
2021-08-23 23:15:01 +0800

    [train] domain cn stages

--- TrainAndTest/train_egs_domain.py
+++ TrainAndTest/train_egs_domain.py
@@ -453,9 +453,9 @@ def main():
             # if args.gain
             # model = DistributedDataParallel(model.cuda(), find_unused_parameters=True)
             # model = DistributedDataParallel(model.cuda())
-            xvector_model = DistributedDataParallel(xvector_model.cuda())
-            classifier_spk = DistributedDataParallel(classifier_spk.cuda())
-            classifier_dom = DistributedDataParallel(classifier_dom.cuda())
+            xvector_model = DistributedDataParallel(xvector_model.cuda(), find_unused_parameters=True)
+            classifier_spk = DistributedDataParallel(classifier_spk.cuda(), find_unused_parameters=True)
+            classifier_dom = DistributedDataParallel(classifier_dom.cuda(), find_unused_parameters=True)
 
         else:
             xvector_model = xvector_model.cuda()
