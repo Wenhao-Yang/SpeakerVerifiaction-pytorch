220c098f14a8a956fc6384a7accc0855797eeb42
william-asus <874681044@qq.com>
2021-08-23 11:23:46 +0800

    [train] domain cn

--- Define_Model/FilterLayer.py
+++ Define_Model/FilterLayer.py
@@ -358,6 +358,33 @@ class GRL(nn.Module):
     def forward(self, x):
         return grl_func.apply(x, self.lambda_)
 
+class RevGrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input_):
+        ctx.save_for_backward(input_)
+        output = input_
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):  # pragma: no cover
+        grad_input = None
+        if ctx.needs_input_grad[0]:
+            grad_input = -grad_output
+        return grad_input
+
+class RevGrad(nn.Module):
+    def __init__(self, *args, **kwargs):
+        """
+        A gradient reversal layer.
+
+        This layer has no parameters, and simply reverses the gradient
+        in the backward pass.
+        """
+
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input_):
+        return RevGrad.apply(input_)
 
 class Inst_Norm(nn.Module):
 
