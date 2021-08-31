e001efa7768ce06f7fac1fd9afdfecac4ac4ad7b
william-asus <874681044@qq.com>
2021-09-01 00:57:05 +0800

    [test] vox1

--- TrainAndTest/test_egs.py
+++ TrainAndTest/test_egs.py
@@ -151,7 +151,7 @@ parser.add_argument('--test-input-per-file', type=int, default=4, metavar='IPFT'
                     help='input sample per file for testing (default: 8)')
 parser.add_argument('--test-batch-size', type=int, default=1, metavar='BST',
                     help='input batch size for testing (default: 64)')
-parser.add_argument('--dropout-p', type=float, default=0.25, metavar='BST',
+parser.add_argument('--dropout-p', type=float, default=0., metavar='BST',
                     help='input batch size for testing (default: 64)')
 
 # loss configure
