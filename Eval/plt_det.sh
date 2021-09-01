a84b002bae59078a63262eb28ae156a13e3cd0d7
Mac-Yang <874681044@qq.com>
2021-09-01 16:44:36 +0800

    [test] aishell2

--- Eval/plt_det.sh
+++ Eval/plt_det.sh
@@ -8,7 +8,7 @@
 
 stage=0
 if [ $stage -le 0 ]; then
-  python Eval/plt_det.py --score-name vox1,aishell \
+  python Eval/plt_det.py --score-name 英语,汉语 \
     --score-file Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var/score.2021.09.01.16\:24\:29,Data/xvector/TDNN_v5/aishell2/spect_egs_baseline/arcsoft_0ce/inputMean_STAP_em512_wde4/aishell2_test_epoch_60_var/score.2021.09.01.16:35:39 \
     --save-path Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var \
     --pf-max 0.1
