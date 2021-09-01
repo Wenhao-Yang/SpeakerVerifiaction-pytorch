0f507a2497895f7d8ce07071897315b433937acb
Mac-Yang <874681044@qq.com>
2021-09-01 16:40:10 +0800

    [test] aishell2

--- Eval/plt_det.sh
+++ Eval/plt_det.sh
@@ -8,8 +8,8 @@
 
 stage=1
 if [ $stage -le 0 ]; then
-  python Eval/plt_det.py --score-name vox1,cnceleb \
-    --score-file Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var/score.2021.09.01.15\:25\:51,Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/cnceleb_test_epoch_50_var/score.2021.09.01.15\:26\:52 \
+  python Eval/plt_det.py --score-name vox1,aishell \
+    --score-file Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var/score.2021.09.01.15\:25\:51,Data/xvector/TDNN_v5/aishell2/spect_egs_baseline/arcsoft_0ce/inputMean_STAP_em512_wde4/aishell2_test_epoch_60_var/score.2021.09.01.16:35:39 \
     --save-path Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var \
     --pf-max 0.1
 
