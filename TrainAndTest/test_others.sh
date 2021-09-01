d6d08b162968329d571fe91c542b6eceada7e423
william-asus <874681044@qq.com>
2021-09-01 21:03:35 +0800

    [train] adv 11 domain

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -965,7 +965,7 @@ if [ $stage -le 83 ]; then
       --train-dir ${lstm_dir}/data/${train_set}/${feat_type}/dev_${feat} \
       --train-test-dir ${lstm_dir}/data/${train_set}/${feat_type}/dev_${feat}/trials_dir \
       --train-trials trials_2w \
-      --trials trials_entertainment \
+      --trials trials \
       --valid-dir ${lstm_dir}/data/${train_set}/${feat_type}/valid_${feat} \
       --test-dir ${lstm_dir}/data/${test_set}/${feat_type}/test_${feat} \
       --feat-format kaldi \
@@ -987,7 +987,6 @@ if [ $stage -le 83 ]; then
       --xvector-dir Data/xvector/${model}/${train_set}/${feat_type}_egs/${loss}/feat${feat}_input${input_norm}_${encod}_em${embedding_size}_wd5e4_var/${test_set}_test_epoch80_var \
       --resume Data/checkpoint/${model}/${train_set}/${feat_type}_egs/${loss}/feat${feat}_input${input_norm}_${encod}_em${embedding_size}_wd5e4_var/checkpoint_80.pth \
       --gpu-id 0 \
-      --extract \
       --cos-sim
   done
   exit
