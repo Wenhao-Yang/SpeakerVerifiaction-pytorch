a85af72c62c7fd2b9977d7ab9207cd2ab2937566
william-asus <874681044@qq.com>
2021-09-03 01:52:08 +0800

    [train] v2 lo baseline

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -1110,14 +1110,14 @@ if [ $stage -le 80 ]; then
       --input-norm ${input_norm} \
       --resnet-size ${resnet_size} \
       --nj 12 \
-      --epochs 50 \
+      --epochs 17 \
       --scheduler rop \
       --patience 2 \
       --accu-steps 1 \
-      --lr 0.1 \
+      --lr 0.0001 \
       --milestones 10,20,30,40 \
       --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${block_type}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}_var \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${block_type}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}_var/checkpoint_8.pth \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${block_type}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}_var/checkpoint_33.pth \
       --kernel-size ${kernel} \
       --channels 64,128,256 \
       --stride 2 \
