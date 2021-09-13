2abb9d33e94aa59821d8719b6b2def3f1d9e7b7b
william-asus <874681044@qq.com>
2021-09-13 23:46:05 +0800

    [process] batch len

--- Define_Model/FilterLayer.py
+++ Define_Model/FilterLayer.py
@@ -588,7 +588,7 @@ class MusanNoiseLayer(nn.Module):
         if not self.training:
             return x
         else:
-            gaussian_noise = torch.normal(mean=self.mean, std=self.std).reshape(1, 1, x.shape[2], 1)
+            gaussian_noise = torch.normal(mean=self.mean, std=self.std).reshape(1, 1, 1, x.shape[3])
             weight = torch.ones(size=(1, 1, x.shape[2], 1))
             torch.nn.init.uniform_(weight, self.weight, 1.25)
 
