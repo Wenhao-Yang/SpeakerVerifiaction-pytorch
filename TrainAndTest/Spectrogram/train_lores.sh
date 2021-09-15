c608e3a6bf48a7bcbf72c635d2c7b358ae3554e7
Mac-Yang <874681044@qq.com>
2021-09-15 21:19:12 +0800

    [train] attention loss

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -1119,8 +1119,8 @@ if [ $stage -le 79 ]; then
       --lr 0.1 \
       --mask-layer ${mask_layer} \
       --milestones 10,20,30,40 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wde3_var \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wde3_var/checkpoint_50.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp25_alpha${alpha}_em${embedding_size}_wde3_var \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp25_alpha${alpha}_em${embedding_size}_wde3_var/checkpoint_50.pth \
       --kernel-size ${kernel} \
       --channels 64,128,256 \
       --stride 2 \
@@ -1137,7 +1137,7 @@ if [ $stage -le 79 ]; then
       --m 3 \
       --loss-ratio 0.01 \
       --weight-decay 0.001 \
-      --dropout-p 0.125 \
+      --dropout-p 0.25 \
       --gpu-id 0,1 \
       --extract \
       --cos-sim \
