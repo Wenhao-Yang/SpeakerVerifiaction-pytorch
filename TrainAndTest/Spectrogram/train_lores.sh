8ad6ed6850570416c6c0b7884d51ef50635e1bf7
william-asus <874681044@qq.com>
2021-09-18 20:13:57 +0800

    [train] additive

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -1186,10 +1186,10 @@ if [ $stage -le 79 ]; then
       --mask-layer ${mask_layer} \
       --init-weight ${weight} \
       --milestones 10,20,30,40 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp25_alpha${alpha}_em${embedding_size}_${weight}_wde3_var \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp25_alpha${alpha}_em${embedding_size}_${weight}_wde3_var/checkpoint_50.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp25_alpha${alpha}_em${embedding_size}_${weight}_wde3_chn32_var \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${block_type}_${encoder_type}_dp25_alpha${alpha}_em${embedding_size}_${weight}_wde3_chn32_var/checkpoint_50.pth \
       --kernel-size ${kernel} \
-      --channels 64,128,256 \
+      --channels 32,64,128 \
       --stride 2 \
       --batch-size 128 \
       --embedding-size ${embedding_size} \
