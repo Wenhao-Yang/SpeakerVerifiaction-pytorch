7eec0fb7f39686340804ff9c12d92394b851a84c
Mac-Yang <874681044@qq.com>
2021-10-25 16:13:03 +0800

    [train] vox2 tdnn

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=90
+stage=93
 waited=0
 while [ $(ps 16447 | wc -l) -eq 2 ]; do
   sleep 60
@@ -1214,4 +1214,68 @@ if [ $stage -le 92 ]; then
 #      --all-iteraion 0 \
 #      --log-interval 10
 #  done
+fi
+
+
+if [ $stage -le 93 ]; then
+  model=RET
+  datasets=vox2
+  feat=log
+  feat_type=klsp
+  loss=arcsoft
+  encod=STAP
+  embedding_size=512
+  block_type=cbam_v2
+  input_norm=Mean
+  batch_size=128
+  resnet_size=14
+  activation=leakyrelu
+  #  --dilation 1,2,3,1 \
+
+  for encod in STAP ; do
+    echo -e "\n\033[1;4;31m Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
+    # kernprof -l -v TrainAndTest/Spectrogram/train_egs.py \
+    python -W ignore TrainAndTest/train_egs.py \
+      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev \
+      --train-test-dir ${lstm_dir}/data/vox1/${feat_type}/dev/trials_dir \
+      --train-trials trials_2w \
+      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_valid \
+      --test-dir ${lstm_dir}/data/vox1/${feat_type}/test \
+      --input-norm ${input_norm} \
+      --nj 12 \
+      --epochs 39 \
+      --patience 2 \
+      --random-chunk 200 400 \
+      --milestones 10,20,30,40,50 \
+      --model ${model} \
+      --resnet-size ${resnet_size} \
+      --block-type ${block_type} \
+      --activation ${activation} \
+      --scheduler rop \
+      --weight-decay 0.00001 \
+      --lr 0.01 \
+      --alpha 0 \
+      --feat-format kaldi \
+      --embedding-size ${embedding_size} \
+      --batch-size ${batch_size} \
+      --accu-steps 1 \
+      --input-dim 161 \
+      --channels 512,512,512,512,512,1536 \
+      --context 5,3,3,5 \
+      --dilation 1,1,1,1 \
+      --stride 1,2,1,1 \
+      --encoder-type ${encod} \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_${encod}_baseline/${loss}/em${embedding_size}_input${input_norm}_${block_type}_${activation}_wde5_stride2_var \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_${encod}_baseline/${loss}/em${embedding_size}_input${input_norm}_${block_type}_${activation}_wde5_stride2_var/checkpoint_21.pth \
+      --cos-sim \
+      --dropout-p 0.0 \
+      --veri-pairs 9600 \
+      --gpu-id 0,1 \
+      --num-valid 2 \
+      --loss-type ${loss} \
+      --margin 0.2 \
+      --s 30 \
+      --all-iteraion 0 \
+      --log-interval 10
+  done
 fi
