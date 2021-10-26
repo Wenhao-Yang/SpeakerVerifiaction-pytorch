59fc10dd57cbef51d6daa4f7aa177c3831800d25
Mac-Yang <874681044@qq.com>
2021-10-26 20:28:52 +0800

    [local] split trials for cnceleb

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -1201,15 +1201,15 @@ if [ $stage -le 83 ]; then
 #      --extract \
 #      --cos-sim
 #  done
-  for s in play recitation singing speech vlog; do
+  for s in recitation singing speech vlog; do
     python -W ignore TrainAndTest/test_egs.py \
       --model ${model} \
       --resnet-size 14 \
       --train-dir ${lstm_dir}/data/${train_set}/${feat_type}/dev_${feat} \
       --train-test-dir ${lstm_dir}/data/${train_set}/${feat_type}/dev_${feat}/trials_dir \
       --train-trials trials_2w \
-      --trials subtrials/trials_play_${s} \
-      --score-suffix pl${s} \
+      --trials subtrials/trials_recitation_${s} \
+      --score-suffix re${s} \
       --valid-dir ${lstm_dir}/data/${train_set}/${feat_type}/valid_${feat} \
       --test-dir ${lstm_dir}/data/${test_set}/${feat_type}/dev_${feat} \
       --feat-format kaldi \
