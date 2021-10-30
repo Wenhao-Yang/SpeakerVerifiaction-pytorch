017016cbe878f280717e18729f4a8e96a573ddf6
william-asus <874681044@qq.com>
2021-10-30 11:28:22 +0800

    [train] v1 lore chn32

--- TrainAndTest/Enhance/train_enhance.sh
+++ TrainAndTest/Enhance/train_enhance.sh
@@ -24,7 +24,7 @@ if [ $stage -le 1 ]; then
       --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/${sname}_valid \
       --feat-format kaldi \
       --input-norm ${input_norm} \
-      --random-chunk 150 250 \
+      --random-chunk 150 400 \
       --resnet-size ${resnet_size} \
       --nj 12 \
       --epochs 40 \
@@ -33,11 +33,11 @@ if [ $stage -le 1 ]; then
       --patience 3 \
       --accu-steps 1 \
       --stride 2,2,2,2 \
-      --lr 0.001 \
+      --lr 0.0005 \
       --milestones 10,20,30,40 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs2_enhance/${loss}/wde5_k5_cyclic_cliplen_var \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs2_enhance/${loss}/wde5_k5_cyclic_cliplen_var/checkpoint_25.pth \
-      --kernel-size 5 \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs2_enhance/${loss}/wde5_cyclic5e4_cliplen_var275 \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs2_enhance/${loss}/wde5_cyclic5e4_cliplen_var275/checkpoint_25.pth \
+      --kernel-size 3 \
       --shuffle \
       --input-dim 40 \
       --batch-size 128 \
