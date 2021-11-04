b9882fc1e784a6972a2346b85f97a907d34b269d
Mac-Yang <874681044@qq.com>
2021-11-04 15:27:56 +0800

    [test] v1 tdnn fb40

--- Define_Model/TDNN/TDNN.py
+++ Define_Model/TDNN/TDNN.py
@@ -933,7 +933,7 @@ class TDNN_v5(nn.Module):
 
         if self.filter_layer != None:
             self.input_dim = feat_dim
-        if self.block_type == 'basic':
+        if self.block_type in ['basic', 'none']:
             TDlayer = TimeDelayLayer_v5
         elif self.block_type == 'basic_v6':
             TDlayer = TimeDelayLayer_v6
