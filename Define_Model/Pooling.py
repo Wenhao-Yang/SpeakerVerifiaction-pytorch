54ef160eed13a135a5fc65f9c08869540359a573
Mac-Yang <874681044@qq.com>
2021-11-23 13:34:12 +0800

    [mdoel] attentive stap

--- Define_Model/Pooling.py
+++ Define_Model/Pooling.py
@@ -98,8 +98,8 @@ class SelfAttentionPooling_v2(nn.Module):
 
         assert x.shape[-1] == self.input_dim
 
-        fx = self.Tanh(self.attention_linear(x))
-        alpha = self.softmax(self.attention_vector(fx))
+        alpha = self.Tanh(self.attention_linear(x))
+        alpha = self.softmax(self.attention_vector(alpha))
 
         mean = torch.sum(alpha * x, dim=1)
 
