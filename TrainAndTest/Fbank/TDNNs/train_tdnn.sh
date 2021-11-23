7d9e74474678dfe995e3ec58fe83bfa126a677cb
Mac-Yang <874681044@qq.com>
2021-11-23 16:05:17 +0800

    [mdoel] attentive stap

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=93
+stage=77
 waited=0
 while [ $(ps 22572 | wc -l) -eq 2 ]; do
   sleep 60
@@ -629,7 +629,7 @@ if [ $stage -le 77 ]; then
   #  feat=fb24
 #  feat_type=pyfb
   feat_type=klfb
-  loss=soft
+  loss=arcsoft
   encod=STAP
   embedding_size=512
   input_dim=40
@@ -638,16 +638,60 @@ if [ $stage -le 77 ]; then
   loss_ratio=0.1
   # _lrr${lr_ratio}_lsr${loss_ratio}
 
-  for loss in arcsoft; do
+#  for loss in arcsoft; do
+#    feat=fb${input_dim}
+#    #_ws25
+#    echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
+#    # kernprof -l -v TrainAndTest/Spectrogram/train_egs.py \
+#    python -W ignore TrainAndTest/train_egs.py \
+#      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev12_${feat} \
+#      --train-test-dir ${lstm_dir}/data/${datasets}/${feat_type}/dev_${feat}/trials_dir \
+#      --train-trials trials_2w \
+#      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev12_${feat}_valid \
+#      --test-dir ${lstm_dir}/data/${datasets}/${feat_type}/test_${feat} \
+#      --nj 12 \
+#      --epochs 60 \
+#      --patience 3 \
+#      --milestones 10,20,30,40 \
+#      --model ${model} \
+#      --scheduler rop \
+#      --weight-decay 0.0005 \
+#      --lr 0.1 \
+#      --alpha 0 \
+#      --feat-format kaldi \
+#      --embedding-size ${embedding_size} \
+#      --batch-size 128 \
+#      --random-chunk 200 400 \
+#      --input-dim ${input_dim} \
+#      --channels 512,512,512,512,1500 \
+#      --encoder-type ${encod} \
+#      --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs12_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var \
+#      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs12_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var/checkpoint_40.pth \
+#      --cos-sim \
+#      --dropout-p 0.0 \
+#      --veri-pairs 9600 \
+#      --gpu-id 0,1 \
+#      --num-valid 2 \
+#      --loss-ratio ${loss_ratio} \
+#      --lr-ratio ${lr_ratio} \
+#      --loss-type ${loss} \
+#      --margin 0.2 \
+#      --s 30 \
+#      --remove-vad \
+#      --log-interval 10
+#  done
+  mask_layer=attention
+
+  for weight in vox2_cf; do
     feat=fb${input_dim}
     #_ws25
     echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
     # kernprof -l -v TrainAndTest/Spectrogram/train_egs.py \
     python -W ignore TrainAndTest/train_egs.py \
-      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev12_${feat} \
+      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_${feat} \
       --train-test-dir ${lstm_dir}/data/${datasets}/${feat_type}/dev_${feat}/trials_dir \
       --train-trials trials_2w \
-      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev12_${feat}_valid \
+      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_${feat}_valid \
       --test-dir ${lstm_dir}/data/${datasets}/${feat_type}/test_${feat} \
       --nj 12 \
       --epochs 60 \
@@ -664,9 +708,11 @@ if [ $stage -le 77 ]; then
       --random-chunk 200 400 \
       --input-dim ${input_dim} \
       --channels 512,512,512,512,1500 \
+      --mask-layer ${mask_layer} \
+      --init-weight ${weight} \
       --encoder-type ${encod} \
-      --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs12_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var \
-      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs12_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var/checkpoint_40.pth \
+      --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${encod}_em${embedding_size}_${weight}_wd5e4_var \
+      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}/${input_norm}_${encod}_em${embedding_size}_${weight}_wd5e4_var/checkpoint_40.pth \
       --cos-sim \
       --dropout-p 0.0 \
       --veri-pairs 9600 \
