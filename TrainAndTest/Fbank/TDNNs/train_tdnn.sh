ad8ffe0c3a2c3ecefefb5976092e13c1c81517af
william-asus <874681044@qq.com>
2021-11-29 00:02:43 +0800

    [train] cnc dist

--- TrainAndTest/Fbank/TDNNs/train_tdnn.sh
+++ TrainAndTest/Fbank/TDNNs/train_tdnn.sh
@@ -688,7 +688,90 @@ if [ $stage -le 77 ]; then
       --test-dir ${lstm_dir}/data/${datasets}/${feat_type}/test_${feat} \
       --nj 12 \
       --shuffle \
-      --epochs 60 \
+      --epochs 29 \
+      --patience 3 \
+      --milestones 10,20,30,40 \
+      --model ${model} \
+      --scheduler rop \
+      --weight-decay 0.0005 \
+      --lr 0.01 \
+      --alpha 0 \
+      --feat-format kaldi \
+      --embedding-size ${embedding_size} \
+      --batch-size 128 \
+      --random-chunk 200 400 \
+      --input-dim ${input_dim} \
+      --channels 512,512,512,512,1500 \
+      --encoder-type ${encod} \
+      --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_lr${loss_ratio}_wd5e4_var \
+      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_lr${loss_ratio}_wd5e4_var/checkpoint_21.pth \
+      --cos-sim \
+      --dropout-p 0.0 \
+      --veri-pairs 9600 \
+      --gpu-id 0,1 \
+      --num-valid 2 \
+      --loss-ratio ${loss_ratio} \
+      --lr-ratio ${lr_ratio} \
+      --loss-type ${loss} \
+      --margin 0.2 \
+      --s 30 \
+      --remove-vad \
+      --log-interval 10
+
+  done
+
+  embedding_size=1024
+  for loss in arcsoft; do
+    feat=fb${input_dim}
+    #_ws25
+    echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
+    # kernprof -l -v TrainAndTest/Spectrogram/train_egs.py \
+#    python -W ignore TrainAndTest/train_egs.py \
+#      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev12_${feat} \
+#      --train-test-dir ${lstm_dir}/data/${datasets}/${feat_type}/dev_${feat}/trials_dir \
+#      --train-trials trials_2w \
+#      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev12_${feat}_valid \
+#      --test-dir ${lstm_dir}/data/${datasets}/${feat_type}/test_${feat} \
+#      --nj 12 \
+#      --shuffle \
+#      --epochs 60 \
+#      --patience 3 \
+#      --milestones 10,20,30,40 \
+#      --model ${model} \
+#      --scheduler rop \
+#      --weight-decay 0.0005 \
+#      --lr 0.1 \
+#      --alpha 0 \
+#      --feat-format kaldi \
+#      --embedding-size ${embedding_size} \
+#      --batch-size 128 \
+#      --random-chunk 200 400 \
+#      --input-dim ${input_dim} \
+#      --channels 512,512,512,512,1500 \
+#      --encoder-type ${encod} \
+#      --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs12_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var \
+#      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs12_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var/checkpoint_40.pth \
+#      --cos-sim \
+#      --dropout-p 0.0 \
+#      --veri-pairs 9600 \
+#      --gpu-id 0,1 \
+#      --num-valid 2 \
+#      --loss-ratio ${loss_ratio} \
+#      --lr-ratio ${lr_ratio} \
+#      --loss-type ${loss} \
+#      --margin 0.2 \
+#      --s 30 \
+#      --remove-vad \
+#      --log-interval 10
+    python -W ignore TrainAndTest/train_egs.py \
+      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_${feat} \
+      --train-test-dir ${lstm_dir}/data/${datasets}/${feat_type}/dev_${feat}/trials_dir \
+      --train-trials trials_2w \
+      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_${feat}_valid \
+      --test-dir ${lstm_dir}/data/${datasets}/${feat_type}/test_${feat} \
+      --nj 12 \
+      --shuffle \
+      --epochs 50 \
       --patience 3 \
       --milestones 10,20,30,40 \
       --model ${model} \
@@ -703,8 +786,8 @@ if [ $stage -le 77 ]; then
       --input-dim ${input_dim} \
       --channels 512,512,512,512,1500 \
       --encoder-type ${encod} \
-      --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_lr${loss_ratio}_wd5e4_var \
-      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_lr${loss_ratio}_wd5e4_var/checkpoint_40.pth \
+      --check-path Data/checkpoint/${model}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var \
+      --resume Data/checkpoint/${model}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${encod}_em${embedding_size}_wd5e4_var/checkpoint_21.pth \
       --cos-sim \
       --dropout-p 0.0 \
       --veri-pairs 9600 \
