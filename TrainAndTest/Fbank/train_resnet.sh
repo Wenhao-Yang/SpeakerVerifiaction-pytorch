3050e94f2193c5fcccdb789e4ae884a4823ecdbf
william-asus <874681044@qq.com>
2021-12-02 21:17:18 +0800

    [train] thinresnet fb40

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -287,11 +287,11 @@ if [ $stage -le 41 ]; then
   datasets=vox1
   feat_type=klfb
   model=ThinResNet
-  resnet_size=18
+  resnet_size=34
   encoder_type=SAP2
   embedding_size=256
   block_type=cbam
-  downsample=k3
+  downsample=None
   kernel=5,5
   loss=arcsoft
   alpha=0
@@ -324,11 +324,11 @@ if [ $stage -le 41 ]; then
       --base-lr 0.000006 \
       --mask-layer ${mask_layer} \
       --milestones 10,20,30,40 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}_${optimizer}_${scheduler}/chn32_${input_norm}_${block_type}_down${downsample}_none1_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wd5e4_var \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}_${optimizer}_${scheduler}/chn32_${input_norm}_${block_type}_down${downsample}_none1_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wd5e4_var/checkpoint_50.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}_${optimizer}_${scheduler}/${input_norm}_${block_type}_down${downsample}_none1_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wd5e4_var \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}_${optimizer}_${scheduler}/${input_norm}_${block_type}_down${downsample}_none1_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wd5e4_var/checkpoint_50.pth \
       --kernel-size ${kernel} \
       --downsample ${downsample} \
-      --channels 32,64,128,256 \
+      --channels 16,32,64,128 \
       --fast none1 \
       --stride 2,1 \
       --block-type ${block_type} \
