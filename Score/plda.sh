4f1d2e79def6981ecc69dcbaf07c3d2eac9eb417
Mac-Yang <874681044@qq.com>
2021-12-10 20:09:52 +0800

    [score] plda
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- Score/plda.sh
+++ Score/plda.sh
@@ -1,38 +1,73 @@
 #!/bin/bash
 # Yangwenhao 2019-12-16 20:27
 
+"""
+Plda score from Python training checkpoint
+"""
 
-model=SuResCNN10
+if [ $# != 4 ]; then
+  echo "Usage: plda.sh <data-path> <train-feat-dir> <test-feat-dir> <trials>"
+  echo "e.g.: plda.sh data/train exp/train exp/test data/test/trials"
+  echo "This script helps create srp to resample wav in wav.scp"
+  exit 1
+fi
+
+data_dir=$1
+train_feat_dir=$2
+test_feat_dir=$3
+trials=$4
+# out_dir=$5
+# model_path=SuResCNN10
+logdir=$test_feat_dir/log
+
 train_cmd="Vector_Score/run.pl --mem 8G"
-logdir=Log/PLDA/${model}
+lda_dim=200
 
-feat_dir=Data/checkpoint/${model}/soft/kaldi_feat
-data_dir=Data/dataset/voxceleb1/kaldi_feat/voxceleb1_test
+# feat_dir=Data/checkpoint/${model}/soft/kaldi_feat
+# data_dir=Data/dataset/voxceleb1/kaldi_feat/voxceleb1_test
 
-trials=$data_dir/trials
-test_score=$feat_dir/scores_voxceleb1_test
+#trials=$data_dir/trials
+test_score=$test_feat_dir/scores_$(date "+%Y-%m-%d-%H:%M:%S")
 
 $train_cmd $logdir/ivector-mean.log \
-    ivector-mean scp:$feat_dir/train_xvector.scp $feat_dir/mean.vec || exit 1;
+    ivector-mean scp:$train_feat_dir/xvectors.scp $test_feat_dir/mean.vec || exit 1;
 
 $train_cmd $logdir/ivector-compute-lda.log \
-    ivector-compute-lda --total-covariance-factor=0.0 --dim=200  "ark:ivector-subtract-global-mean scp:$feat_dir/train_xvector.scp ark:- |" ark:$feat_dir/utt2spk $feat_dir/transform.mat || exit 1;
+    ivector-compute-lda --total-covariance-factor=0.0 --dim=$lda_dim  "ark:ivector-subtract-global-mean scp:$train_feat_dir/xvectors.scp ark:- |" ark:$data_dir/utt2spk $train_feat_dir/transform.mat || exit 1;
 
-Score/utt2spk_to_spk2utt.pl $feat_dir/utt2spk > $feat_dir/spk2utt
+
+# if ! [ -f $data_dir/spk2utt ];then
+#     Score/utt2spk_to_spk2utt.pl $data_dir/utt2spk > $data_dir/spk2utt
+# fi
+
+if ! [ -f $train_feat_dir/utt2spk ];then
+    cat $train_feat_dir/xvectors.scp | awk '{print $1}' > $train_feat_dir/utt
+    grep -f $train_feat_dir/utt $data_dir/utt2spk > $train_feat_dir/utt2spk
+
+    Score/utt2spk_to_spk2utt.pl $train_feat_dir/utt2spk > $$train_feat_dir/spk2utt
+fi
+
 
 $train_cmd $logdir/ivector-compute-plda.log \
-    ivector-compute-plda ark:$feat_dir/spk2utt "ark:ivector-subtract-global-mean scp:$feat_dir/train_xvector.scp ark:- | transform-vec $feat_dir/transform.mat ark:- ark:- | ivector-normalize-length ark:-  ark:- |" $feat_dir/plda || exit 1;
+    ivector-compute-plda ark:$data_dir/spk2utt "ark:ivector-subtract-global-mean scp:$train_feat_dir/xvectors.scp ark:- | transform-vec $train_feat_dir/transform.mat ark:- ark:- | ivector-normalize-length ark:-  ark:- |" $train_feat_dir/plda || exit 1;
+
 
 $train_cmd $logdir/ivector-plda-scoring.log \
     ivector-plda-scoring --normalize-length=true \
-    "ivector-copy-plda --smoothing=0.0 $feat_dir/plda - |" \
-    "ark:ivector-subtract-global-mean $feat_dir/mean.vec scp:$feat_dir/test_xvector.scp ark:- | transform-vec $feat_dir/transform.mat ark:- ark:- | ivector-normalize-length ark:- ark:- |" \
-    "ark:ivector-subtract-global-mean $feat_dir/mean.vec scp:$feat_dir/test_xvector.scp ark:- | transform-vec $feat_dir/transform.mat ark:- ark:- | ivector-normalize-length ark:- ark:- |" \
-    "cat '$trials' | cut -d\  --fields=1,2 |" $feat_dir/scores_voxceleb1_test || exit 1;
+    "ivector-copy-plda --smoothing=0.0 $train_feat_dir/plda - |" \
+    "ark:ivector-subtract-global-mean $train_feat_dir/mean.vec scp:$test_feat_dir/xvectors.scp ark:- | transform-vec $train_feat_dir/transform.mat ark:- ark:- | ivector-normalize-length ark:- ark:- |" \
+    "ark:ivector-subtract-global-mean $train_feat_dir/mean.vec scp:$test_feat_dir/xvectors.scp ark:- | transform-vec $train_feat_dir/transform.mat ark:- ark:- | ivector-normalize-length ark:- ark:- |" \
+    "cat '$trials' | cut -d\  --fields=1,2 |" $test_score || exit 1;
 
 eer=`compute-eer <(Score/prepare_for_eer.py $trials $test_score) 2> /dev/null`
 mindcf1=`Score/compute_min_dcf.py --p-target 0.01 $test_score $trials 2> /dev/null`
 mindcf2=`Score/compute_min_dcf.py --p-target 0.001 $test_score $trials 2> /dev/null`
+
+test_result=$test_feat_dir/result_$(date "+%Y-%m-%d-%H:%M:%S")
+
+echo "EER: $eer%" >> $test_result
+echo "minDCF(p-target=0.01): $mindcf1" >> $test_result
+echo "minDCF(p-target=0.001): $mindcf2" >> $test_result
 
 echo "EER: $eer%"
 echo "minDCF(p-target=0.01): $mindcf1"
