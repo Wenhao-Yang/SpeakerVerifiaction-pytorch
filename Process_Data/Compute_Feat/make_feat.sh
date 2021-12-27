d783ffcc3c8c5a54cb748d901250845229e3ec4a
Mac-Yang <874681044@qq.com>
2021-12-27 21:52:42 +0800

    [test] output thin34 cnc drop

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -48,16 +48,18 @@ fi
 #stage=1000
 if [ $stage -le 1 ]; then
 #  dataset=vox2
-  # dataset=cnceleb
-  dataset=aishell2
+  dataset=cnceleb
+#  dataset=aishell2
   #  feat_type=pyfb
   #  dataset=vox1
-  feat=klfb
-  feat_type=klfb
+  feat=klsp
+  feat_type=klsp
+#        --remove-vad \
+
 
   echo -e "\n\033[1;4;31m Stage ${stage}: making ${feat} egs for ${dataset}\033[0m\n"
   #  for s in dev_log dev_aug_1m_log ; do
-  for s in dev_fb40; do
+  for s in dev dev12; do
     python Process_Data/Compute_Feat/make_egs.py \
       --data-dir ${lstm_dir}/data/${dataset}/${feat}/${s} \
       --out-dir ${lstm_dir}/data/${dataset}/egs/${feat} \
@@ -69,7 +71,6 @@ if [ $stage -le 1 ]; then
       --feat-format kaldi \
       --out-format kaldi_cmp \
       --num-valid 2 \
-      --remove-vad \
       --out-set ${s}
 
     python Process_Data/Compute_Feat/make_egs.py \
@@ -82,7 +83,6 @@ if [ $stage -le 1 ]; then
       --feat-format kaldi \
       --out-format kaldi_cmp \
       --num-valid 2 \
-      --remove-vad \
       --out-set ${s}_valid
   done
   exit
