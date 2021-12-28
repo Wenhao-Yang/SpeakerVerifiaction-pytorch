f8be7e371369afd6661bb15f081c320dcb638888
Mac-Yang <874681044@qq.com>
2021-12-28 22:01:03 +0800

    [train] v1 drop baseline batch256

--- TrainAndTest/Fbank/train_tdnn.sh
+++ TrainAndTest/Fbank/train_tdnn.sh
@@ -2,7 +2,7 @@
 
 stage=70
 waited=0
-while [ $(ps 1348898 | wc -l) -eq 2 ]; do
+while [ $(ps 2420416 | wc -l) -eq 2 ]; do
   sleep 60
   waited=$(expr $waited + 1)
   echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
