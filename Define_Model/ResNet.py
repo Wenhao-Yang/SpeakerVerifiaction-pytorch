037fe62dde40ac596b61f5b1663f2d54c60a4a69
william-asus <874681044@qq.com>
2022-01-11 01:09:48 +0800

    [train] thinresnet sap2 vox22 KLSP

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -93,6 +93,57 @@ class SEBasicBlock(nn.Module):
 
         return out
 
+
+class SEBasicBlock_v2(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None, reduction_ratio=2, **kwargs):
+        super(SEBasicBlock_v2, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        if stride == 1:
+            self.conv1 = conv3x3(inplanes, planes, stride)
+        elif stride == 2:
+            self.conv1 = conv5x5(inplanes, planes, stride)
+
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.reduction_ratio = reduction_ratio
+
+        # Squeeze-and-Excitation
+        self.se_layer = SqueezeExcitation(inplanes=planes, reduction_ratio=reduction_ratio)
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.se_layer(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
 class CBAMBlock(nn.Module):
     expansion = 1
 
@@ -137,6 +188,57 @@ class CBAMBlock(nn.Module):
         out = self.relu(out)
 
         return out
+
+
+class CBAMBlock_v2(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None, reduction_ratio=2, **kwargs):
+        super(CBAMBlock_v2, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        if stride == 1:
+            self.conv1 = conv3x3(inplanes, planes, stride)
+        elif stride == 2:
+            self.conv1 = conv5x5(inplanes, planes, stride)
+
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+        self.reduction_ratio = reduction_ratio
+
+        # Squeeze-and-Excitation
+        self.CBAM_layer = CBAM(planes, planes)
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out = self.CBAM_layer(out)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
 
 class Res2Conv2dReluBn(nn.Module):
     '''
@@ -560,15 +662,18 @@ class ThinResNet(nn.Module):
         if block_type == "seblock":
             block = SEBasicBlock
             # block.reduction_ratio = red_ratio
+        elif block_type == "seblock_v2":
+            block = SEBasicBlock_v2
         elif block_type == 'cbam':
             block = CBAMBlock
+        elif block_type == 'cbam_v2':
+            block = CBAMBlock_v2
         elif block_type in ['basic', 'None']:
             block = BasicBlock if resnet_size < 50 else Bottleneck
         elif block_type == 'basic_v2':
             block = BasicBlock_v2
         elif block_type == 'seres2block':
             block = SE_Res2Block
-
 
         block.expansion = expansion
         # num_filter = [32, 64, 128, 256]
