01947716d5657ef51ff1820e1a2fead12ec4b186
william-asus <874681044@qq.com>
2022-01-18 15:24:03 +0800

    [train] thinresnet sap2 cnc

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -844,7 +844,7 @@ if [ $stage -le 101 ]; then
       --s 30 \
       --weight-decay 0.0005 \
       --dropout-p 0.1 \
-      --gpu-id 2,3 \
+      --gpu-id 3,4 \
       --extract \
       --cos-sim \
       --all-iteraion 0 \
