fa30a656a07d94e30bf25fa6ea475d59613e42f2
william-asus <874681044@qq.com>
2022-01-18 15:09:18 +0800

    [train] thinresnet sap2 cnc

--- TrainAndTest/train_egs.py
+++ TrainAndTest/train_egs.py
@@ -625,7 +625,7 @@ def main():
         if len(args.gpu_id) > 1:
             print("Continue with gpu: %s ..." % str(args.gpu_id))
             torch.distributed.init_process_group(backend="nccl",
-                                                 init_method='file:///home/ssd2020/yangwenhao/lstm_speaker_verification/data/sharedfile2',
+                                                 init_method='file:///home/yangwenhao/lstm_speaker_verification/data/sharedfile',
                                                  rank=0,
                                                  world_size=1)
             # if args.gain
