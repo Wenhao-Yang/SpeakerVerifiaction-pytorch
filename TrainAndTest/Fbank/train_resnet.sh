cfac62137a9db3c0425b783f945740b6e11d39ed
Mac-Yang <874681044@qq.com>
2022-01-20 14:44:16 +0800

    [train] thin34 fb80

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -666,7 +666,7 @@ if [ $stage -le 100 ]; then
        --s 30 \
        --weight-decay 0.0005 \
        --dropout-p 0.1 \
-       --gpu-id 0,1 \
+       --gpu-id 3,4,5 \
        --extract \
        --cos-sim \
        --all-iteraion 0 \
@@ -844,7 +844,7 @@ if [ $stage -le 101 ]; then
       --s 30 \
       --weight-decay 0.0005 \
       --dropout-p 0.1 \
-      --gpu-id 3,4 \
+      --gpu-id 3,4,5 \
       --extract \
       --cos-sim \
       --all-iteraion 0 \
