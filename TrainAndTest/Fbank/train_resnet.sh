4b635e7c1c138794c86958b6b1d94a5d1ef59580
Mac-Yang <874681044@qq.com>
2022-01-20 14:40:32 +0800

    [train] thin34 fb80

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -630,11 +630,11 @@ if [ $stage -le 100 ]; then
      python TrainAndTest/train_egs.py \
        --model ${model} \
        --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev${subset}_fb${input_dim} \
-       --train-test-dir ${lstm_dir}/data/vox1/${feat_type}/dev_fb${input_dim}/trials_dir \
+       --train-test-dir ${lstm_dir}/data/${testset}/${feat_type}/dev_fb${input_dim}/trials_dir \
        --train-trials trials_2w \
        --shuffle \
        --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev${subset}_fb${input_dim}_valid \
-       --test-dir ${lstm_dir}/data/vox1/${feat_type}/test_fb${input_dim} \
+       --test-dir ${lstm_dir}/data/${testset}/${feat_type}/test_fb${input_dim} \
        --feat-format kaldi \
        --random-chunk 200 400 \
        --input-norm ${input_norm} \
