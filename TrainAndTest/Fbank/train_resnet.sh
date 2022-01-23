eb3c10fabe741515ace6ca2f39ef6f3841236a1a
william-asus <874681044@qq.com>
2022-01-23 10:16:53 +0800

    [train] thinresnet sap2 klfb 24 40 64 80

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -318,7 +318,7 @@ if [ $stage -le 40 ]; then
       --random-chunk 200 400 \
       --input-norm ${input_norm} \
       --resnet-size ${resnet_size} \
-      --nj 12 \
+      --nj 9 \
       --epochs 50 \
       --batch-size ${batch_size} \
       --optimizer ${optimizer} \
