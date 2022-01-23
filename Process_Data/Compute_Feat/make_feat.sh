5b284fdf82ace9bf548179d2d7c2adb52eaad3a6
william-asus <874681044@qq.com>
2022-01-24 02:37:41 +0800

    [train] thinresnet sap2 klfb 24 40 64 80

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -7,7 +7,7 @@ lstm_dir=/home/yangwenhao/project/lstm_speaker_verification
 # echo $(awk '{n += $2} END{print n}' <utt2num_frames)
 
 waited=0
-while [ $(ps 12841 | wc -l) -eq 2 ]; do
+while [ $(ps 261004 | wc -l) -eq 2 ]; do
   sleep 60
   waited=$(expr $waited + 1)
   echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
@@ -51,15 +51,16 @@ if [ $stage -le 1 ]; then
 #  dataset=cnceleb
 #  dataset=aishell2
   #  feat_type=pyfb
-  dataset=vox1
-  feat=klfb
-  feat_type=klfb
+  dataset=vox2
+  feat=klsp
+  feat_type=klsp
 #        --remove-vad \
 
 
   echo -e "\n\033[1;4;31m Stage ${stage}: making ${feat} egs for ${dataset}\033[0m\n"
   #  for s in dev_log dev_aug_1m_log ; do
-  for s in dev_fb24 dev_fb40 dev_f64 dev_fb80; do
+#  for s in dev_fb24 dev_fb40 dev_f64 dev_fb80; do
+  for s in dev; do
     python Process_Data/Compute_Feat/make_egs.py \
       --data-dir ${lstm_dir}/data/${dataset}/${feat}/${s} \
       --out-dir ${lstm_dir}/data/${dataset}/egs/${feat} \
@@ -71,7 +72,6 @@ if [ $stage -le 1 ]; then
       --feat-format kaldi \
       --out-format kaldi_cmp \
       --num-valid 2 \
-      --remove-vad \
       --out-set ${s}
 
     python Process_Data/Compute_Feat/make_egs.py \
@@ -84,7 +84,6 @@ if [ $stage -le 1 ]; then
       --feat-format kaldi \
       --out-format kaldi_cmp \
       --num-valid 2 \
-      --remove-vad \
       --out-set ${s}_valid
   done
   exit
