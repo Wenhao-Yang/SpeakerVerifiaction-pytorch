fa00301c803b1ba3b28ecea81518e31925865208
Mac-Yang <874681044@qq.com>
2022-02-09 16:58:24 +0800

    [test] v1 klsp file thin34

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=96
+stage=201
 lstm_dir=/home/work2020/yangwenhao/project/lstm_speaker_verification
 
 # ===============================    LoResNet10    ===============================
@@ -2020,6 +2020,100 @@ fi
 
 
 if [ $stage -le 201 ]; then
+  feat_type=klsp
+  model=ThinResNet
+  feat=log
+  loss=arcsoft
+  encod=SAP2
+  alpha=0
+  datasets=vox1
+  testset=vox1
+#  test_subset=
+  block_type=cbam_v2
+#  encoder_type=None
+  embedding_size=256
+  resnet_size=34
+#  sname=dev #dev_aug_com
+  sname=dev #_aug_com
+  downsample=k5
+  test_subset=test
+
+  for sname in dev dev_aug_com; do
+    echo -e "\n\033[1;4;31mStage ${stage}: Testing ${model}_${resnet_size} in ${datasets} with ${loss} kernel 5,5 \033[0m\n"
+    python -W ignore TrainAndTest/test_egs.py \
+      --model ${model} \
+      --resnet-size ${resnet_size} \
+      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/${sname} \
+      --train-test-dir ${lstm_dir}/data/vox1/${feat_type}/dev/trials_dir \
+      --train-trials trials_2w \
+      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/${sname}_valid \
+      --test-dir ${lstm_dir}/data/${testset}/${feat_type}/${test_subset} \
+      --feat-format kaldi \
+      --input-norm Mean \
+      --input-dim 161 \
+      --nj 12 \
+      --embedding-size ${embedding_size} \
+      --loss-type ${loss} \
+      --fast none1 \
+      --downsample ${downsample} \
+      --encoder-type ${encod} \
+      --block-type ${block_type} \
+      --kernel-size 5,5 \
+      --stride 2,2 \
+      --channels 32,64,128,256 \
+      --alpha ${alpha} \
+      --margin 0.2 \
+      --s 30 \
+      --time-dim 1 \
+      --avg-size 5 \
+      --input-length var \
+      --dropout-p 0.1 \
+      --xvector-dir Data/xvector//ThinResNet34/vox1/klsp_egs_rvec/arcsoft_sgd_rop/chn32_Mean_cbam_v2_downk5_SAP2_em256_dp01_alpha0_none1_wd5e4_var_${sname}/${test_subset}_epoch_60_var \
+      --resume Data/checkpoint/ThinResNet34/vox1/klsp_egs_rvec/arcsoft_sgd_rop/chn32_Mean_cbam_v2_downk5_SAP2_em256_dp01_alpha0_none1_wd5e4_var_${sname}/checkpoint_60.pth \
+      --gpu-id 0 \
+      --cos-sim
+
+#    python -W ignore TrainAndTest/test_egs.py \
+#      --model ${model} \
+#      --resnet-size ${resnet_size} \
+#      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/${sname} \
+#      --train-test-dir ${lstm_dir}/data/vox1/${feat_type}/dev/trials_dir \
+#      --train-trials trials_2w \
+#      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/${sname}_valid \
+#      --test-dir ${lstm_dir}/data/${testset}/${feat_type}/${test_subset} \
+#      --feat-format kaldi \
+#      --input-norm Mean \
+#      --input-dim 161 \
+#      --nj 12 \
+#      --mask-layer attention \
+#      --init-weight vox2 \
+#      --embedding-size ${embedding_size} \
+#      --loss-type ${loss} \
+#      --fast none1 \
+#      --downsample ${downsample} \
+#      --encoder-type ${encod} \
+#      --block-type ${block_type} \
+#      --kernel-size 5,5 \
+#      --stride 2,2 \
+#      --channels 16,32,64,128 \
+#      --alpha ${alpha} \
+#      --margin 0.2 \
+#      --s 30 \
+#      --time-dim 1 \
+#      --avg-size 5 \
+#      --input-length var \
+#      --dropout-p 0.1 \
+#      --xvector-dir Data/xvector/ThinResNet${resnet_size}/vox1/klsp_egs_rvec_attention/arcsoft/inputMean_basic_v2_downk5_AVG_em256_dp125_alpha0_none1_vox2_wd5e4_var/${test_subset}_epoch_50_var \
+#      --resume Data/checkpoint/ThinResNet${resnet_size}/vox1/klsp_egs_rvec_attention/arcsoft/inputMean_basic_v2_downk5_AVG_em256_dp125_alpha0_none1_vox2_wd5e4_var/checkpoint_50.pth \
+#      --gpu-id 0 \
+#      --cos-sim
+  done
+  exit
+
+fi
+
+
+if [ $stage -le 202 ]; then
   feat_type=klfb
   model=ThinResNet
   feat=log
