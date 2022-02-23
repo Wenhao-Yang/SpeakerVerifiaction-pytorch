2ed608c707ad6cb179a05bdf13ff5f8b95b4ebc2
Mac-Yang <874681044@qq.com>
2022-02-23 16:14:08 +0800

    [test] resnet 10

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -54,7 +54,7 @@ if [ $stage -le 1 ]; then
   dataset=vox1
   feat=klsp
   feat_type=klsp
-  num_frames=400
+  num_frames=600
 #        --remove-vad \
 
   echo -e "\n\033[1;4;31m Stage ${stage}: making ${feat} egs for ${dataset}\033[0m\n"
