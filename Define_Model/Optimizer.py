0335778c8230d6c11a928ed645f3b8d08eab7c8e
Mac-Yang <874681044@qq.com>
2022-03-07 19:25:18 +0800

    [extract] common cnc

--- Define_Model/Optimizer.py
+++ Define_Model/Optimizer.py
@@ -55,9 +55,14 @@ class SAMSGD(SGD):
             closure: A closure that reevaluates the model and returns the loss.
         Returns: the loss value evaluated on the original point
         """
+        loss = None
         if closure is not None:
             with torch.enable_grad():
-                loss = closure().detach()
+                loss = closure()
+
+        # if closure is not None:
+        #     with torch.enable_grad():
+        #         loss = closure().detach()
 
         for group in self.param_groups:
             grads = []
