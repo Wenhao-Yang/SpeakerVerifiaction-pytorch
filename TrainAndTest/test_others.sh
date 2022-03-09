a364f8dc7f00e9083f4aaf3371754ee113b6b156
Mac-Yang <874681044@qq.com>
2022-03-09 11:16:24 +0800

    [train] common cnc 2.3s

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -2663,10 +2663,11 @@ if [ $stage -le 301 ]; then
        --num-frames 200 \
        --gpu-id 0 \
        --verbose 2 \
-       --extract \
        --cos-sim
    done
        # --verbose 2 \
+#              --extract \
+
 
  # for s in adve_adve adve_spee dram_reci ente_reci inte_sing live_vlog play_sing sing_vlog adve_dram adve_vlog dram_sing ente_sing inte_spee movi_movi play_spee spee_spee adve_ente dram_spee ente_spee inte_vlog movi_play play_vlog spee_vlog adve_inte dram_dram dram_vlog ente_vlog live_live movi_reci reci_reci vlog_vlog adve_live dram_ente ente_ente inte_inte live_movi movi_sing reci_sing adve_movi dram_inte ente_inte inte_live live_play movi_spee reci_spee adve_play dram_live ente_live inte_movi live_reci movi_vlog reci_vlog adve_reci dram_movi ente_movi inte_play live_sing play_play sing_sing adve_sing dram_play ente_play inte_reci live_spee play_reci sing_spee; do
 # for s in adad adpl drdr drre enli envl insi lire more plsi revl vlvl addr adre dren drsi enmo inin insp lisi mosi plsp sisi aden adsi drin drsp enpl inli invl lisp mosp plvl sisp adin adsp drli drvl enre inmo lili livl movl rere sivl adli advl drmo enen ensi inpl limo momo plpl resi spsp admo drpl enin ensp inre lipl mopl plre resp spvl; do
