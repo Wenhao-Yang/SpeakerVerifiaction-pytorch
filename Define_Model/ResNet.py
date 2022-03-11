3a15c2bcc22f3762d860518b1b4552225f06d163
Mac-Yang <874681044@qq.com>
2022-03-11 23:03:42 +0800

    [train] arc nosie pad 400

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -302,7 +302,7 @@ class SE_Res2Block(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.downsample = downsample
         self.conv1 = Conv2dReluBn(inplanes, planes, kernel_size=1, stride=1, padding=0),
-        self.conv2 = Res2Conv2dReluBn(planes, kernel_size, stride, padding, dilation, scale=scale),
+        self.conv2 = Res2Conv2dReluBn(planes, kernel_size, padding, dilation, stride=1, scale=scale),
         self.conv3 = Conv2dReluBn(planes, planes, kernel_size=1, stride=1, padding=0),
 
         # Squeeze-and-Excitation
