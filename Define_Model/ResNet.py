4e360b6c08d5b6599d6db9ddf7e4ca8792b6ff2f
Mac-Yang <874681044@qq.com>
2022-03-13 18:37:34 +0800

    [train] arc nosie pad 400

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -302,7 +302,7 @@ class SE_Res2Block(nn.Module):
 
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.downsample = downsample
-        self.conv1 = Conv2dReluBn(inplanes, planes, kernel_size=1, stride=1, padding=0)
+        self.conv1 = Conv2dReluBn(inplanes, planes, kernel_size=1, stride=stride, padding=0)
         self.conv2 = Res2Conv2dReluBn(planes, kernel_size, padding, dilation, stride=1, scale=scale)
         self.conv3 = Conv2dReluBn(planes, planes, kernel_size=1, stride=1, padding=0)
 
@@ -321,7 +321,7 @@ class SE_Res2Block(nn.Module):
         if self.downsample is not None:
             identity = self.downsample(x)
 
-        pdb.set_trace()
+        # pdb.set_trace()
         out += identity
         out = F.relu(out)
 
