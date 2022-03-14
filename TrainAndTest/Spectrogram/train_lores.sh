28c7930a31ffad233b6fa9c1005900d0eabbe00a
Mac-Yang <874681044@qq.com>
2022-03-14 20:20:13 +0800

    [data] vox1 female male

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=170
+stage=78
 
 waited=0
 while [ $(ps 17765 | wc -l) -eq 2 ]; do
@@ -1193,44 +1193,64 @@ fi
 
 
 if [ $stage -le 78 ]; then
-  lstm_dir=/home/work2020/yangwenhao/project/lstm_speaker_verification
   datasets=vox1
   feat_type=klsp
   model=LoResNet
   resnet_size=8
-  encoder_type=None
+  encoder_type=AVG
   embedding_size=256
   block_type=cbam
   kernel=5,5
   loss=arcsoft
   alpha=0
   input_norm=Mean
-  mask_layer=gau_noise
-  for mask_layer in None; do
+#  mask_layer=gau_noise
+  mask_layer=baseline
+
+  scheduler=rop
+  optimizer=sgd
+
+  for subset in female male ; do
+    chn=64
+    if [ $chn -eq 64 ];then
+      channels=64,128,256
+      dp=0.25
+      dp_str=25
+    elif [ $chn -eq 32 ];then
+      channels=32,64,128
+      dp=0.2
+      dp_str=20
+    elif [ $chn -eq 16 ];then
+      channels=16,32,64
+      dp=0.125
+      dp_str=125
+    fi
     echo -e "\n\033[1;4;31m Stage${stage}: Training ${model}${resnet_size} in ${datasets}_egs with ${loss} with ${input_norm} normalization \033[0m\n"
     python TrainAndTest/train_egs.py \
       --model ${model} \
-      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev \
+      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_${subset} \
       --train-test-dir ${lstm_dir}/data/vox1/${feat_type}/dev/trials_dir \
       --train-trials trials_2w \
-      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_valid \
+      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_${subset}_valid \
       --test-dir ${lstm_dir}/data/vox1/${feat_type}/test \
       --feat-format kaldi \
       --random-chunk 200 400 \
+      --input-dim 161 \
       --input-norm ${input_norm} \
       --resnet-size ${resnet_size} \
       --nj 12 \
       --epochs 50 \
-      --scheduler rop \
+      --optimizer ${optimizer} \
+      --scheduler ${scheduler} \
       --patience 2 \
       --accu-steps 1 \
       --lr 0.1 \
       --mask-layer ${mask_layer} \
       --milestones 10,20,30,40 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${block_type}_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wd5e4_chn16_var \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs_baseline/${loss}/${input_norm}_${block_type}_${encoder_type}_dp125_alpha${alpha}_em${embedding_size}_wd5e4_chn16_var/checkpoint_50.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${input_norm}_${block_type}_${encoder_type}_dp${dp_str}_alpha${alpha}_em${embedding_size}_chn${chn}_wd5e4_var \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${input_norm}_${block_type}_${encoder_type}_dp${dp_str}_alpha${alpha}_em${embedding_size}_chn${chn}_wd5e4_var/checkpoint_50.pth \
       --kernel-size ${kernel} \
-      --channels 16,32,64 \
+      --channels ${channels} \
       --stride 2 \
       --batch-size 128 \
       --embedding-size ${embedding_size} \
@@ -1245,7 +1265,7 @@ if [ $stage -le 78 ]; then
       --m 3 \
       --loss-ratio 0.01 \
       --weight-decay 0.0005 \
-      --dropout-p 0.125 \
+      --dropout-p ${dp} \
       --gpu-id 0,1 \
       --extract \
       --cos-sim \
