6e613ce5123a34ac1d172a8b3258503ada05189c
Mac-Yang <874681044@qq.com>
2022-03-17 18:49:31 +0800

    [data] cnc klfb24 cut klsp

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -48,12 +48,12 @@ fi
 #stage=1000
 if [ $stage -le 1 ]; then
 #  dataset=vox2
-#  dataset=cnceleb
+  dataset=cnceleb
 #  dataset=aishell2
   #  feat_type=pyfb
   dataset=vox1
-  feat=klsp
-  feat_type=klsp
+  feat=klfb
+  feat_type=klfb
 
 #  feat=klfb
 #  feat_type=klfb
@@ -62,7 +62,7 @@ if [ $stage -le 1 ]; then
 
   echo -e "\n\033[1;4;31m Stage ${stage}: making ${feat} egs for ${dataset}\033[0m\n"
   #  for s in dev_log dev_aug_1m_log ; do
-  for s in dev_male ; do
+  for s in dev_fb24_cut ; do
     python Process_Data/Compute_Feat/make_egs.py \
       --data-dir ${lstm_dir}/data/${dataset}/${feat}/${s} \
       --out-dir ${lstm_dir}/data/${dataset}/egs/${feat} \
