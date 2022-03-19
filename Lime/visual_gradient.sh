03cc4c82fdaa9e0a1f1c654643531be977d8e70b
Mac-Yang <874681044@qq.com>
2022-03-19 20:03:15 +0800

    [lime] thin18 vox1

--- Lime/visual_gradient.sh
+++ Lime/visual_gradient.sh
@@ -180,10 +180,11 @@ if [ $stage -le 111 ]; then
   # vox1 Data/gradient/LoResNet8/vox1/klsp_egs_baseline/arcsoft/None_cbam_em256_alpha0_dp25_wd5e4_${s}_var/epoch_40_var_40/epoch_40 \
   # Data/gradient/LoResNet8/vox1/klsp_egs_baseline/arcsoft/None_cbam_em256_alpha0_dp25_wd5e4_dev_var/epoch_40_var2/epoch_40
   # Data/gradient/LoResNet8/vox1/klsp_egs_baseline/arcsoft/None_cbam_em256_alpha0_dp25_wd5e4_dev_var/epoch_40_var2/epoch_40
+  # Data/gradient/LoResNet8/vox1/klsp_egs${s}_baseline/arcsoft_sgd_rop/Mean_cbam_AVG_dp20_alpha0_em256_chn32_wd5e4_var/epoch_50_var/epoch_50
 
-  for s in female male  ;do
+  for s in female   ;do
     python Lime/Plot/visual_gradient.py \
-      --extract-path Data/gradient/LoResNet8/vox1/klsp_egs${s}_baseline/arcsoft_sgd_rop/Mean_cbam_AVG_dp20_alpha0_em256_chn32_wd5e4_var/epoch_50_var/epoch_50 \
+      --extract-path Data/gradient/ThinResNet18/vox1/klsp_egs_rvec/arcsoft_sgd_rop/Mean_cbam_v2_downk5_SAP2_em256_dp01_alpha0_none1_wd5e4_var_dev/epoch_60_var_gradient/epoch_60 \
       --feat-dim 161 \
       --acoustic-feature spectrogram
   done
