5421bbbf498a103eeb52ee92ed7b1695cfa99930
Mac-Yang <874681044@qq.com>
2022-03-23 13:31:00 +0800

    [train] xvector

--- TrainAndTest/train_egs_binary_frame.py
+++ TrainAndTest/train_egs_binary_frame.py
@@ -643,6 +643,7 @@ def train(train_loader, model, ce, optimizer, epoch, scheduler, steps):
             domain_embeddings = spk_embeddings.detach()
 
         for i in range(steps):
+            pdb.set_trace()
             dom_logits = classifier_dom(domain_embeddings)
             dom_loss = xe_criterion(dom_logits, true_labels_b)
 
