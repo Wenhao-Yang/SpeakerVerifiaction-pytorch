cf25359e4b1dc06797fb09da8dd086d2ef4314ce
william-asus <874681044@qq.com>
2022-03-26 12:56:28 +0800

    [train] loresnet rand one

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -1310,7 +1310,7 @@ if [ $stage -le 79 ]; then
     dp_str=125
   fi
 
-  for weight in rand one ; do
+  for weight in rand ; do
     echo -e "\n\033[1;4;31m Stage${stage}: Training ${model}${resnet_size} in ${datasets}_egs with ${loss} with ${input_norm} normalization \033[0m\n"
     python TrainAndTest/train_egs.py \
       --model ${model} \
