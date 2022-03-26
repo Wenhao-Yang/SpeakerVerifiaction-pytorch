dddc5de2133deb5fc2c034386630fafc0ffc1cdc
Mac-Yang <874681044@qq.com>
2022-03-26 23:40:52 +0800

    [xvector] resnet18

--- TrainAndTest/test_egs.py
+++ TrainAndTest/test_egs.py
@@ -682,6 +682,7 @@ if __name__ == '__main__':
     if args.test:
         file_loader = kaldiio.load_mat
         # file_loader = read_vec_flt
+        return_uid = True if args.score_norm != '' else False
         test_dir = ScriptVerifyDataset(dir=args.test_dir, trials_file=args.trials, xvectors_dir=args.xvector_dir,
                                        loader=file_loader, return_uid=args.return_uid)
 
