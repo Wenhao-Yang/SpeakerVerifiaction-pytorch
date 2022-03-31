4d54aabbf314e197af8bdb700558e74682ae3d7d
Mac-Yang <874681044@qq.com>
2022-03-31 16:46:51 +0800

    [model] ecapa sinc

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -298,18 +298,18 @@ if [ $stage -le 21 ]; then
   #  for s in dev_log dev_aug_1m_log ; do
 #  for s in dev_fb24 dev_fb40 dev_f64 dev_fb80; do
   for s in train_c20 ; do
-    python Process_Data/Compute_Feat/make_egs.py \
-      --data-dir ${lstm_dir}/data/${dataset}/${feat}/${s} \
-      --out-dir ${lstm_dir}/data/${dataset}/egs/${feat} \
-      --nj ${nj} \
-      --feat-type ${feat_type} \
-      --train \
-      --input-per-spks ${input_per_spks} \
-      --num-frames ${num_frames} \
-      --feat-format kaldi \
-      --out-format kaldi_cmp \
-      --num-valid 2 \
-      --out-set ${s}
+#    python Process_Data/Compute_Feat/make_egs.py \
+#      --data-dir ${lstm_dir}/data/${dataset}/${feat}/${s} \
+#      --out-dir ${lstm_dir}/data/${dataset}/egs/${feat} \
+#      --nj ${nj} \
+#      --feat-type ${feat_type} \
+#      --train \
+#      --input-per-spks ${input_per_spks} \
+#      --num-frames ${num_frames} \
+#      --feat-format kaldi \
+#      --out-format kaldi_cmp \
+#      --num-valid 2 \
+#      --out-set ${s}
 
     python Process_Data/Compute_Feat/make_egs.py \
       --data-dir ${lstm_dir}/data/${dataset}/${feat}/${s} \
