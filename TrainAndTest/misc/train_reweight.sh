f652617dd39cd7d594662af6419011ba01f78683
Mac-Yang <874681044@qq.com>
2022-04-16 15:55:13 +0800

    [test]dev1 dev12

--- TrainAndTest/misc/train_reweight.sh
+++ TrainAndTest/misc/train_reweight.sh
@@ -108,7 +108,7 @@ if [ $stage -le 0 ]; then
       --num-center 3 \
       --weight-decay 0.0005 \
       --dropout-p 0.1 \
-      --gpu-id 2,3 \
+      --gpu-id 0,1 \
       --extract \
       --cos-sim \
       --all-iteraion 0 \
