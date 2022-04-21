17b404b80c155711984f2934f7e5cd5c285f9900
william-asus <874681044@qq.com>
2022-04-22 00:44:57 +0800

    [common] np.concatenate

--- TrainAndTest/common_func.py
+++ TrainAndTest/common_func.py
@@ -324,10 +324,9 @@ def verification_extract(extract_loader, model, xvector_dir, epoch, test_input='
     # print("uid2vectors size is :", len(uid2vectors))
     # print(all_uid2vectors[-1])
     all_uid2vectors = [None for _ in range(torch.distributed.get_world_size())]
-    torch.distributed.gather_object(
+    torch.distributed.all_gather_object(
         uid2vectors,
         all_uid2vectors if torch.distributed.get_rank() == 0 else None,
-        dst=0
     )
     if torch.distributed.get_rank() == 0:
 
