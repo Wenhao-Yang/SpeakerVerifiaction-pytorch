4342edb071f5c67d41ba2a2a47896588263cb0f2
Mac-Yang <874681044@qq.com>
2022-05-08 22:41:49 +0800

    [data] cnc 12 dp01 egs 3s

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -806,7 +806,7 @@ if [ $stage -le 101 ]; then
       --nj 12 \
       --epochs 60 \
       --target-ratio 0.2 \
-      --inter-ratio 0.5 \
+      --inter-ratio 0.25 \
       --batch-size ${batch_size} \
       --optimizer ${optimizer} \
       --scheduler ${scheduler} \
