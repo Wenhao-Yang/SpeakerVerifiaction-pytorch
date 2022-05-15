9c74d4c9e6788a36fa86f87f117442a9c4bf6593
Mac-Yang <874681044@qq.com>
2022-05-15 11:40:42 +0800

    [train] early stopping

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -985,7 +985,7 @@ if [ $stage -le 102 ]; then
   model=ThinResNet
   resnet_size=34
   encoder_type=SAP2
-  embedding_size=256
+  embedding_size=512
   block_type=basic
   downsample=k3
   kernel=5,5
@@ -1057,8 +1057,8 @@ if [ $stage -le 102 ]; then
       --mask-layer ${mask_layer} \
       --mask-len ${mask_len} \
       --milestones 10,20,30,40,50 \
-      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${chn_str}${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}${loss_str}_wd5e4_var_espatience10 \
-      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${chn_str}${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}${loss_str}_wd5e4_var_espatience10/checkpoint_30.pth \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${chn_str}${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}${loss_str}_wd5e4_var_es \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${chn_str}${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}${loss_str}_wd5e4_var_es/checkpoint_30.pth \
       --kernel-size ${kernel} \
       --downsample ${downsample} \
       --channels ${channels} \
@@ -1073,6 +1073,118 @@ if [ $stage -le 102 ]; then
       --alpha ${alpha} \
       --margin 0.2 \
       --s 30 \
+      --weight-decay 0.0005 \
+      --dropout-p 0.1 \
+      --gpu-id 0,3 \
+      --extract \
+      --cos-sim \
+      --all-iteraion 0 \
+      --remove-vad \
+      --loss-ratio ${loss_ratio} \
+      --stat-type ${stat_type} \
+      --loss-type ${loss}
+  done
+  exit
+fi
+
+
+if [ $stage -le 102 ]; then
+  lstm_dir=/home/yangwenhao/project/lstm_speaker_verification
+  datasets=cnceleb
+  testset=cnceleb
+  feat_type=klfb
+  model=ThinResNet
+  resnet_size=34
+  encoder_type=SAP2
+  embedding_size=512
+  block_type=basic
+  downsample=k3
+  kernel=5,5
+  loss=arcsoft
+  alpha=0
+  input_norm=Mean
+  mask_layer=None
+  scheduler=rop
+  optimizer=sgd
+  input_dim=40
+  batch_size=256
+  fast=none1
+  mask_layer=both
+  weight=one
+  scale=0.2
+  weight_p=0.1584
+  subset=12
+  mask_len=5,5
+  stat_type=maxmargin
+  chn=16
+  loss_ratio=1
+        # --milestones 15,25,35,45 \
+
+  for loss in aDCF ; do
+    echo -e "\n\033[1;4;31m Stage${stage}: Training ${model}${resnet_size} in ${datasets}_egs with ${loss} with ${input_norm} normalization \033[0m\n"
+#${weight}_scale${scale}dp${weight_p}
+#      --init-weight ${weight} \
+#      --scale ${scale} \
+#      --weight-p ${weight_p} \
+    if [ "$loss" == "arcdist" ];then
+      loss_str=_${stat_type}lr${loss_ratio}
+    else
+      loss_str=
+    fi
+
+    if [ $chn -eq 16 ]; then
+      channels=16,32,64,128
+      chn_str=
+    elif [ $chn -eq 32 ]; then
+      channels=32,64,128,256
+      chn_str=chn32_
+    fi
+
+    python TrainAndTest/train_egs.py \
+      --model ${model} \
+      --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev${subset}_fb${input_dim} \
+      --train-test-dir ${lstm_dir}/data/${datasets}/${feat_type}/test_fb${input_dim} \
+      --train-trials trials \
+      --shuffle \
+      --valid-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev${subset}_fb${input_dim}_valid \
+      --test-dir ${lstm_dir}/data/${testset}/${feat_type}/test_fb${input_dim} \
+      --feat-format kaldi \
+      --random-chunk 200 400 \
+      --input-norm ${input_norm} \
+      --input-dim ${input_dim} \
+      --resnet-size ${resnet_size} \
+      --nj 12 \
+      --epochs 60 \
+      --batch-size ${batch_size} \
+      --optimizer ${optimizer} \
+      --scheduler ${scheduler} \
+      --patience 2 \
+      --early-stopping \
+      --early-patience 15 \
+      --early-delta 0.001 \
+      --early-meta MinDCF_01 \
+      --lr 0.1 \
+      --base-lr 0.000001 \
+      --mask-layer ${mask_layer} \
+      --mask-len ${mask_len} \
+      --milestones 10,20,30,40,50 \
+      --check-path Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${chn_str}${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}${loss_str}_wd5e4_var_es \
+      --resume Data/checkpoint/${model}${resnet_size}/${datasets}/${feat_type}_egs${subset}_${mask_layer}/${loss}_${optimizer}_${scheduler}/${chn_str}${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}${loss_str}_wd5e4_var_es/checkpoint_30.pth \
+      --kernel-size ${kernel} \
+      --downsample ${downsample} \
+      --channels ${channels} \
+      --fast ${fast} \
+      --stride 2,1 \
+      --block-type ${block_type} \
+      --embedding-size ${embedding_size} \
+      --time-dim 1 \
+      --avg-size 0 \
+      --encoder-type ${encoder_type} \
+      --num-valid 2 \
+      --alpha ${alpha} \
+      --margin 0.2 \
+      --smooth-ratio 0.25 \
+      --s 40 \
       --weight-decay 0.0005 \
       --dropout-p 0.1 \
       --gpu-id 0,3 \
