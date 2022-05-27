6861275318a6d24f7555d2487ce905f5e9c5184b
Mac-Yang <874681044@qq.com>
2022-05-27 23:12:37 +0800

    [train] ecapa dist

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -37,16 +37,16 @@ def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
 
-def conv3x3(in_planes, out_planes, stride=1):
+def conv3x3(in_planes, out_planes, stride=1, groups: int = 1, dilation: int = 1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1,
-                     stride=stride, bias=False)
+                     stride=stride, groups=groups, bias=False, dilation=dilation)
 
 
-def conv5x5(in_planes, out_planes, stride=2, groups=1):
+def conv5x5(in_planes, out_planes, stride=2, groups=1, dilation=1):
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=5, padding=2,
-                     stride=stride, bias=False, groups=groups)
+                     stride=stride, bias=False, groups=groups, dilation=dilation)
 
 
 class SEBasicBlock(nn.Module):
@@ -657,6 +657,70 @@ class BasicBlock_v2(nn.Module):
         return out
 
 
+class Bottleneck_v2(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(
+            self,
+            inplanes: int,
+            planes: int,
+            stride: int = 1,
+            downsample: Optional[nn.Module] = None,
+            groups: int = 1,
+            base_width: int = 64,
+            dilation: int = 1,
+            norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+
+        if stride == 1:
+            self.conv2 = conv3x3(width, width, stride, groups, dilation)
+            # self.conv2 = conv3x3(inplanes, planes, stride)
+        elif stride == 2:
+            self.conv1 = conv5x5(width, width, stride, groups, dilation)
+
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
 class ThinResNet(nn.Module):
 
     def __init__(self, resnet_size=34, block_type='None', expansion=1, channels=[16, 32, 64, 128],
@@ -709,6 +773,10 @@ class ThinResNet(nn.Module):
             block = CBAMBlock_v2
         elif block_type in ['basic', 'None']:
             block = BasicBlock if resnet_size < 50 else Bottleneck
+        elif block_type in ['bottle']:
+            block = Bottleneck
+        elif block_type in ['bottle_v2']:
+            block = Bottleneck_v2
         elif block_type == 'basic_v2':
             block = BasicBlock_v2
         elif block_type == 'se2block':
