6fd5625acf7b227b326e908c1123700d401d150d
Mac-Yang <874681044@qq.com>
2022-06-07 19:52:18 +0800

    [model] proser mixup

--- TrainAndTest/train_egs_proser.py
+++ TrainAndTest/train_egs_proser.py
@@ -185,7 +185,7 @@ def train(train_loader, model, ce, optimizer, epoch, scheduler):
         shuf_half_idx_ten = torch.LongTensor(half_idx)
 
         select_bool = half_label != half_label[shuf_half_idx_ten]
-        select_bool = select_bool.repeat_interleave(half_feats.shape[1], dim=1)
+        select_bool = select_bool.reshape(-1, 1).repeat_interleave(half_feats.shape[1], dim=1)
         # torch.repeat_interleave()
         half_a_feat = torch.masked_select(half_feats, mask=select_bool)
         half_b_feat = torch.masked_select(half_feats[shuf_half_idx_ten],
