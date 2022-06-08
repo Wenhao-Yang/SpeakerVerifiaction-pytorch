976d5dedeb6edbbf32c568ed9e09e4c717cab06d
Mac-Yang <874681044@qq.com>
2022-06-08 19:25:38 +0800

    [model] proser mixup

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -349,7 +349,7 @@ if [ $stage -le 40 ]; then
       --s 30 \
       --weight-decay 0.0005 \
       --dropout-p 0.1 \
-      --gpu-id 1,2 \
+      --gpu-id 1 \
       --extract \
       --cos-sim \
       --all-iteraion 0 \
