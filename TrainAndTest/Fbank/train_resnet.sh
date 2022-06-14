c693bb046882e1ffde818371961d1b9f22ee37a3
Mac-Yang <874681044@qq.com>
2022-06-14 15:32:26 +0800

    [model] proser mixup

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -239,7 +239,7 @@ if [ $stage -le 40 ]; then
   model=ThinResNet
   resnet_size=34
   encoder_type=SAP2
-  embedding_size=256
+  embedding_size=512
   block_type=cbam
   downsample=k3
   kernel=5,5
