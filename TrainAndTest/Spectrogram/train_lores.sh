a0ec8c124de0e29cba5afb78199f5ffe66c2f2ad
Mac-Yang <874681044@qq.com>
2022-07-21 12:18:46 +0800

    [train] seed test

--- TrainAndTest/Spectrogram/train_lores.sh
+++ TrainAndTest/Spectrogram/train_lores.sh
@@ -1300,7 +1300,7 @@ if [ $stage -le 79 ]; then
   weight=clean
   for weight in mel clean aug vox2 ; do
   for chn in 16 32 ; do
-  for seed in 1234567 123458; do
+  for seed in 123457 ; do
     echo -e "\n\033[1;4;31m Stage${stage}: Training ${model}${resnet_size} in ${datasets}_egs with ${loss} with ${input_norm} normalization \033[0m\n"
     if [ $chn -eq 64 ];then
     channels=64,128,256
