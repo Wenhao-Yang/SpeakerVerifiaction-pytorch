21a81bebcc9091b600563945a5dd76d057738e69
Mac-Yang <874681044@qq.com>
2022-08-31 16:07:47 +0800

    [mdoel] ECAPA bias conv

--- TrainAndTest/Spectrogram/train_tdnn.sh
+++ TrainAndTest/Spectrogram/train_tdnn.sh
@@ -49,7 +49,7 @@ if [ $stage -le 0 ]; then
       chn_str=chn1024_
     fi
     echo -e "\n\033[1;4;31mStage ${stage}: Training ${model} in ${datasets}_egs with ${loss} \033[0m\n"
-    model_dir=${model}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}_${optimizer}_${scheduler}/${input_norm}_batch${batch_size}_${block_type}_${encoder_type}_em${embedding_size}_${chn_str}wde5_var_025/${seed}
+    model_dir=${model}/${datasets}/${feat_type}_egs_${mask_layer}/${loss}_${optimizer}_${scheduler}/${input_norm}_batch${batch_size}_${block_type}_${encoder_type}_em${embedding_size}_${chn_str}wde5_var/${seed}
     python TrainAndTest/train_egs.py \
       --model ${model} \
       --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/${sname} \
