2b0735699b3ca86c9191929b2d9471cb91f1e55e
Mac-Yang <874681044@qq.com>
2022-08-31 19:51:16 +0800

    [mdoel] ECAPA bias conv

--- TrainAndTest/Spectrogram/train_kd.sh
+++ TrainAndTest/Spectrogram/train_kd.sh
@@ -302,8 +302,8 @@ if [ $stage -le 10 ]; then
   kd_loss=
   chn=16
 #  _${weight}
-  for chn in 16 ; do
-  for seed in 123456 123457 123458; do
+  for kd_type in attention embedding_cos embedding_mse ; do
+  for seed in 123456 ; do
 
      if [ $resnet_size -le 34 ];then
         expansion=1
@@ -323,6 +323,12 @@ if [ $stage -le 10 ]; then
         chn_str=chn64_
       fi
       if [[ $mask_layer == attention* ]];then
+        kd_ratio=1000
+      else
+        kd_ratio=0.4
+      fi
+
+      if [[ $kd_type == attention ]];then
         at_str=_${weight}
         if [[ $weight_norm != max ]];then
           at_str=${at_str}${weight_norm}
@@ -356,7 +362,7 @@ if [ $stage -le 10 ]; then
        --nj ${nj} \
        --shuffle \
        --epochs 50 \
-       --batch-size 128 \
+       --batch-size ${batch_size} \
        --optimizer ${optimizer} \
        --scheduler ${scheduler} \
        --lr 0.1 \
