c0a4233fed531c8598e5271dbcd2c67f9ac1b22d
Mac-Yang <874681044@qq.com>
2022-08-31 13:02:32 +0800

    [lime] acc_grad

--- Lime/cam_extract.py
+++ Lime/cam_extract.py
@@ -472,10 +472,10 @@ def train_extract(train_loader, model, file_dir, set_name, save_per_num=2500):
                         # this_grad = out_layer_grad[i].clone().cpu()
                         this_feat = out_layer_feat[-1 - i].clone().cpu()
 
-                        # try:
-                        this_grad = ups(this_feat).mean(dim=1, keepdim=True)
-                        # except Exception as e:
-                        #     print(this_feat.shape)
+                        try:
+                            this_grad = ups(this_feat).mean(dim=1, keepdim=True)
+                        except Exception as e:
+                            print(this_feat.shape)
 
                         this_grad = this_grad.clamp_min(0)
                         this_grad /= this_grad.max()
