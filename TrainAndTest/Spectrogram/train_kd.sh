29ad4f5173e53376cf857c415187703a16f0dacd
Mac-Yang <874681044@qq.com>
2022-09-04 09:15:21 +0800

    [train] train kd freq

--- TrainAndTest/Spectrogram/train_kd.sh
+++ TrainAndTest/Spectrogram/train_kd.sh
@@ -305,7 +305,7 @@ if [ $stage -le 10 ]; then
   chn=16
 #  _${weight}
   for kd_type in attention ; do
-  for seed in 123456 ; do
+  for seed in 123457 123458 ; do
 
      if [ $resnet_size -le 34 ];then
         expansion=1
