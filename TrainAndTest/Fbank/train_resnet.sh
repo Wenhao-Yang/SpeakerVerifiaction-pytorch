9edce46c3388357f6d715452a1a23e6bba66620b
Mac-Yang <874681044@qq.com>
2022-09-05 16:42:28 +0800

    [mdoel] fast resnet

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -437,7 +437,7 @@ if [ $stage -le 41 ]; then
     else
       at_str=
     fi
-    model_dir=${model}${resnet_size}/${datasets}/${feat_type}${input_dim}_egs_${mask_layer}/${seed}/${loss}_${optimizer}_${scheduler}/${input_norm}_batch${batch_size}_${block_type}_down${downsample}_avg${avg_size}_${encoder_type}_em${embedding_size}_dp00_alpha${alpha}_${fast}${at_str}_${chn_str}wde4_vares/${seed}
+    model_dir=${model}${resnet_size}/${datasets}/${feat_type}${input_dim}_egs_${mask_layer}/${seed}/${loss}_${optimizer}_${scheduler}/${input_norm}_batch${batch_size}_${block_type}_down${downsample}_avg${avg_size}_${encoder_type}_em${embedding_size}_dp01_alpha${alpha}_${fast}${at_str}_${chn_str}wde4_vares/${seed}
     # _reduct${red_ratio}
     python TrainAndTest/train_egs.py \
       --model ${model} \
@@ -485,7 +485,7 @@ if [ $stage -le 41 ]; then
       --margin 0.2 \
       --s 30 \
       --weight-decay 0.0001 \
-      --dropout-p 0.0 \
+      --dropout-p 0.1 \
       --gpu-id 1,2 \
       --extract \
       --cos-sim \
