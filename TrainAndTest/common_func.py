36c452cfa6bd32a6276a112da0ae71d29e7b169b
Mac-Yang <874681044@qq.com>
2022-09-11 16:51:01 +0800

    [train] kd time both

--- TrainAndTest/common_func.py
+++ TrainAndTest/common_func.py
@@ -766,6 +766,8 @@ def args_parse(description: str = 'PyTorch Speaker Recognition: Classification')
 
         parser.add_argument('--mvnorm', action='store_true', default=False,
                             help='need to make spectrograms file')
+        parser.add_argument('--valid', action='store_true', default=False,
+                            help='need to make spectrograms file')
         # parser.add_argument('--mean-vector', action='store_false', default=True,
         #                     help='mean for embeddings while extracting')
         parser.add_argument('--score-norm', type=str, default='', help='score normalization')
