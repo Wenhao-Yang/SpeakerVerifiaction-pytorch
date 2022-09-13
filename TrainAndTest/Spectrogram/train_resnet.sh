0be2e2fd2a8873e2ba019e78efe8304243b10f6d
Mac-Yang <874681044@qq.com>
2022-09-13 21:01:48 +0800

    [train] kd time both

--- TrainAndTest/Spectrogram/train_resnet.sh
+++ TrainAndTest/Spectrogram/train_resnet.sh
@@ -569,7 +569,7 @@ if [ $stage -le 41 ]; then
         --lr-ratio 0.01 \
         --weight-decay 0.0001 \
         --dropout-p 0.1 \
-        --gpu-id 5 \
+        --gpu-id 0 \
         --all-iteraion 0 \
         --extract \
         --cos-sim \
