acf5dabf3ae1c009cf5a6ee2ea1af502c29b2d98
Mac-Yang <874681044@qq.com>
2022-09-17 11:39:58 +0800

    [train] early stop mix eer mindcf

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -264,6 +264,8 @@ if [ $stage -le 40 ]; then
   dummy=100
 
   for input_dim in 40; do
+  for seed in 123456 ; do
+
     if [ $resnet_size -le 34 ];then
       expansion=1
       batch_size=256
@@ -274,7 +276,7 @@ if [ $stage -le 40 ]; then
     fi
     model_dir=${model}${resnet_size}/${datasets}/${feat_type}${input_dim}_egs_${mask_layer}/${loss}_${optimizer}_${scheduler}/${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}_wd5e4_vares_dummy${dummy}_beta${proser_ratio}_gamma${proser_gamma}/${seed}
     echo -e "\n\033[1;4;31m Stage${stage}: Training ${model}${resnet_size} in ${datasets}_egs with ${loss} with ${input_norm} normalization \033[0m\n"
-    python TrainAndTest/train_egs.py \
+    python TrainAndTest/train_egs_proser.py \
       --model ${model} --resnet-size ${resnet_size} \
       --train-dir ${lstm_dir}/data/${datasets}/egs/${feat_type}/dev_fb${input_dim} \
       --train-test-dir ${lstm_dir}/data/${testset}/${feat_type}/test_fb${input_dim} \
@@ -308,6 +310,7 @@ if [ $stage -le 40 ]; then
       --extract \
       --cos-sim \
       --remove-vad
+  done
   done
   exit
 fi
