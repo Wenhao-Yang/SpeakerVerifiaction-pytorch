10110547264f4ecd25f9e2981e3a41f899164774
Mac-Yang <874681044@qq.com>
2022-09-24 14:40:33 +0800

    [train] resnet mixup proser

--- Process_Data/Compute_Feat/make_egs.py
+++ Process_Data/Compute_Feat/make_egs.py
@@ -325,11 +325,11 @@ if __name__ == "__main__":
     lock_i = manager.Lock()
     lock_t = manager.Lock()
 
-    print(train_dir.__getitem__(1)[0].shape)
-    feat_dim = train_dir.__getitem__(1)[0].shape[-1]
+    # print(train_dir.__getitem__(1)[0].shape)
+    feat_shape = train_dir.__getitem__(1)[0].shape
     mem_data = psutil.virtual_memory()
     free_mem = mem_data.available
-    maxsize = int(free_mem / (args.num_frames * feat_dim * 4) * 0.5)
+    maxsize = int(free_mem / (feat_shape[0] * feat_shape[1] * 4) * 0.5)
     maxsize = min(maxsize, 150000)
     maxsize = max(maxsize, 1500)
     print('Maxsize for Queue is %d' % maxsize)
