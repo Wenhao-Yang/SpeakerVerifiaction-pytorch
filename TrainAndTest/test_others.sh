37621af4da3b21fc6c49fe4360fbaeb993761f05
Mac-Yang <874681044@qq.com>
2022-09-27 13:44:32 +0800

    [train] fbank mixup 2

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -2555,7 +2555,7 @@ if [ $stage -le 203 ]; then
       valid_dir=dev_fb${input_dim}_valid
     fi
 #    Mean_batch256_seblock_downk1_none1_ASTP2_dp01_alpha1_em256_wd5e4_vares_bashuf2_dummy100_beta1_gamma0.01
-    model_dir=${model}${resnet_size}/${datasets}/${feat_type}${input_dim}_egs_${mask_layer}/${loss}_${optimizer}_${scheduler}/${input_norm}_batch${batch_size}_${block_type}_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}_wd5e4_vares_bashuf2/${seed}
+    model_dir=${model}${resnet_size}/${datasets}/${feat_type}${input_dim}_egs_${mask_layer}/${loss}_${optimizer}_${scheduler}/${input_norm}_batch${batch_size}_${block_type}_red2_down${downsample}_${fast}_${encoder_type}_dp01_alpha${alpha}_em${embedding_size}_wd5e4_vares_bashuf2/${seed}
 #    _dummy${dummy}_beta${proser_ratio}_gamma${proser_gamma}
     python -W ignore TrainAndTest/test_egs.py \
       --model ${model} --resnet-size ${resnet_size} \
