f70f01250640a583ab2c5eedc0f10abf668d8fbe
Mac-Yang <874681044@qq.com>
2022-09-29 21:01:44 +0800

    [train] fbank mixup 2

--- TrainAndTest/train_egs_mixup.py
+++ TrainAndTest/train_egs_mixup.py
@@ -256,10 +256,19 @@ def train(train_loader, model, ce, optimizer, epoch, scheduler):
 
                 loss = (1 - alpha_t) * loss + alpha_t * predict_loss + args.beta * torch.mean(-torch.log(mp))
 
-            minibatch_correct = lamda_beta * predicted_one_labels.eq(
-                label[:len(predicted_one_labels)]).cpu().sum().float() + \
-                                (1 - lamda_beta) * predicted_one_labels.eq(
-                label[-len(predicted_one_labels):]).cpu().sum().float()
+            if args.mixup_type == 'manifold':
+                minibatch_correct = predicted_one_labels[:half_data].eq(
+                    label[:half_data]).cpu().sum().float() + \
+                                    lamda_beta * predicted_one_labels[half_data:].eq(
+                    label[half_data:(half_data + half_data)]).cpu().sum().float() + \
+                                    (1 - lamda_beta) * predicted_one_labels.eq(
+                    label[-half_data:]).cpu().sum().float()
+            else:
+                minibatch_correct = lamda_beta * predicted_one_labels.eq(
+                    label[:len(predicted_one_labels)]).cpu().sum().float() + \
+                                    (1 - lamda_beta) * predicted_one_labels.eq(
+                    label[-len(predicted_one_labels):]).cpu().sum().float()
+
             minibatch_acc = minibatch_correct / len(predicted_one_labels)
             batch_accs.append(minibatch_acc)
 
