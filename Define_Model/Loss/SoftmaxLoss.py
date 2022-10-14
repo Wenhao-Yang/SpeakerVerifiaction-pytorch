d3a7214857abcd937bdc52bb6adc5a7568c0dc8f
Mac-Yang <874681044@qq.com>
2022-10-14 21:08:47 +0800

    [train] v2 fb40 egs

--- Define_Model/Loss/SoftmaxLoss.py
+++ Define_Model/Loss/SoftmaxLoss.py
@@ -799,10 +799,14 @@ class MixupLoss(nn.Module):
     def forward(self, costh, label, half_batch_size, lamda_beta):
         # pdb.set_trace()
         loss = self.loss(costh[:half_batch_size], label[:half_batch_size])
+        # print(loss)
 
         loss = loss + self.gamma * (
-                    lamda_beta * self.loss(costh[-half_batch_size:], label[half_batch_size:int(2 * half_batch_size)]) \
-                    + (1 - lamda_beta) * self.loss(costh[-half_batch_size:], label[-half_batch_size:]))
+                lamda_beta * self.loss(costh[-half_batch_size:], label[half_batch_size:int(2 * half_batch_size)]) \
+                + (1 - lamda_beta) * self.loss(costh[-half_batch_size:], label[-half_batch_size:]))
+        if torch.isnan(loss):
+            pdb.set_trace()
+            print(loss)
 
         return loss / (1 + self.gamma)
 
