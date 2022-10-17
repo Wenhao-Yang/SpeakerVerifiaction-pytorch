39ded581e8820e044a1428f6beba4d052f6b18bc
Mac-Yang <874681044@qq.com>
2022-10-17 17:20:32 +0800

    [data] vox1 wave

--- TrainAndTest/Wav/train_filter.sh
+++ TrainAndTest/Wav/train_filter.sh
@@ -395,15 +395,16 @@ if [ $stage -le 20 ]; then
   encod=ASTP2
   embedding_size=256
   # _lrr${lr_ratio}_lsr${loss_ratio}
+  for lamda_beta in 0.2 0.5 1.0 2.0;do
+    for seed in 123456 123457 ; do
+     feat=fb${input_dim}
 
- for seed in 123456 123457 ; do
-   feat=fb${input_dim}
-
-   echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
-#   CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py
-#   CUDA_VISIBLE_DEVICES=3,5 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417410 --nnodes=1 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/aidata_resnet.yaml --seed=${seed}
-#   CUDA_VISIBLE_DEVICES=5,6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417430 --nnodes=1 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Wav/vox1_resnet.yaml --seed=${seed}
-   CUDA_VISIBLE_DEVICES=5,6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417430 --nnodes=1 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Wav/vox1_resnet_mixup.yaml --seed=${seed}
+     echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
+    #   CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py
+    #   CUDA_VISIBLE_DEVICES=3,5 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417410 --nnodes=1 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/aidata_resnet.yaml --seed=${seed}
+    #   CUDA_VISIBLE_DEVICES=5,6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417430 --nnodes=1 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Wav/vox1_resnet.yaml --seed=${seed}
+     CUDA_VISIBLE_DEVICES=5,6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417430 --nnodes=1 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Wav/vox1_resnet_mixup.yaml --seed=${seed} --lamda-beta ${lamda_beta}
+    done
   done
   exit
 fi
--- TrainAndTest/train_egs_dist_mixup.py
+++ TrainAndTest/train_egs_dist_mixup.py
@@ -81,6 +81,7 @@ parser.add_argument('--local_rank', default=-1, type=int,
 
 parser.add_argument('--train-config', default='', type=str, help='node rank for distributed training')
 parser.add_argument('--seed', type=int, default=123456, help='random seed (default: 0)')
+parser.add_argument('--lamda-beta', type=float, default=2.0, help='random seed (default: 0)')
 
 args = parser.parse_args()
 
@@ -111,7 +112,7 @@ with open(args.train_config, 'r') as f:
 
 # create logger
 # Define visulaize SummaryWriter instance
-check_path = config_args['check_path'] + '/' + str(args.seed)
+check_path = config_args['check_path'] + '_lamda' + str(args.lamda_beta) + '/' + str(args.seed)
 
 if torch.distributed.get_rank() == 0:
     if not os.path.exists(check_path):
@@ -565,7 +566,7 @@ def main():
     # test_display_triplet_distance = False
     # print the experiment configuration
     torch.distributed.barrier()
-    check_path = config_args['check_path'] + '/' + str(args.seed)
+    check_path = config_args['check_path'] + '_lamda' + str(args.lamda_beta) + '/' + str(args.seed)
 
     if torch.distributed.get_rank() == 0:
         print('\nCurrent time: \33[91m{}\33[0m.'.format(str(time.asctime())))
