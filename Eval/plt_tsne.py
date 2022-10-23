b2c6aec694e25b445d43959f48e193f684e1a79d
Mac-Yang <874681044@qq.com>
2022-10-23 23:15:00 +0800

    [data] vox1 wave 1234

--- Eval/plt_tsne.py
+++ Eval/plt_tsne.py
@@ -28,13 +28,24 @@ parser.add_argument('--sid-length', default=7, type=int,
 parser.add_argument('--num-spk', default=7, type=int,
                     help='num of speakers to plot (default: 10)')
 parser.add_argument('--out-pdf', default='', type=str, help='num of speakers to plot (default: 10)')
+parser.add_argument('--distance', default='l2', type=str, help='num of speakers to plot (default: 10)')
 
 args = parser.parse_args()
+
+
+def cos(x, y):
+    costh = x * y / (np.sqrt((x ** 2).sum()) * np.sqrt((y ** 2).sum()))
+    return 1 - costh.sum()
+
+
+def l2(x, y):
+    return np.sqrt(((x - y) ** 2).sum())
+
 
 if __name__ == '__main__':
 
     vects = {}
-    with ReadHelper('scp:%s'% args.scp_file) as reader:
+    with ReadHelper('scp:%s' % args.scp_file) as reader:
         for key, numpy_array in reader:
             vects[key] = numpy_array
 
@@ -59,43 +70,57 @@ if __name__ == '__main__':
 
     all_vectors = []
     all_len = [0]
-    for spk in spks_this:
-        spk_con = np.concatenate(spk2vec[spk])
-        all_len.append(len(spk_con))
-        all_vectors.append(spk_con)
 
-    all_vectors = np.concatenate(all_vectors, axis=0)
-    S_embedded = TSNE(n_components=2).fit_transform(all_vectors)
+    if args.num_spk > 0:
+        for spk in spks_this:
+            spk_con = np.concatenate(spk2vec[spk])
+            all_len.append(len(spk_con))
+            all_vectors.append(spk_con)
 
-    emb_group = []
-    for i in range(len(all_len) - 1):
-        start = np.sum(all_len[:(i + 1)]).astype(np.int32)
-        stop = np.sum(all_len[:(i + 2)]).astype(np.int32)
-        this_points = S_embedded[start:stop]
-        assert len(this_points) > 0, 'start:stop is %s:%s' % (start, stop)
-        emb_group.append(this_points)
+        all_vectors = np.concatenate(all_vectors, axis=0)
+        S_embedded = TSNE(n_components=2).fit_transform(all_vectors)
 
-    plt.figure(figsize=(8, 8))
-    leng = []
-    for idx, group in enumerate(emb_group):
-        if len(group) > 0:
-            c = cValue_1[idx]
-            leng.append(spks_this[idx])
-            plt.scatter(group[:, 0], group[:, 1], color=c, s=10)
+        emb_group = []
+        for i in range(len(all_len) - 1):
+            start = np.sum(all_len[:(i + 1)]).astype(np.int32)
+            stop = np.sum(all_len[:(i + 2)]).astype(np.int32)
+            this_points = S_embedded[start:stop]
+            assert len(this_points) > 0, 'start:stop is %s:%s' % (start, stop)
+            emb_group.append(this_points)
 
-    # plt.legend(leng, loc="best", fontsize=18)
-    # plt.xlim([])
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
-    if args.out_pdf.endswith('pdf'):
-        plt.savefig(args.out_pdf, format="pdf")
+        plt.figure(figsize=(8, 8))
+        leng = []
+        for idx, group in enumerate(emb_group):
+            if len(group) > 0:
+                c = cValue_1[idx]
+                leng.append(spks_this[idx])
+                plt.scatter(group[:, 0], group[:, 1], color=c, s=10)
 
-    plt.show()
+        # plt.legend(leng, loc="best", fontsize=18)
+        # plt.xlim([])
+        plt.xticks(fontsize=16)
+        plt.yticks(fontsize=16)
+        if args.out_pdf.endswith('pdf'):
+            plt.savefig(args.out_pdf, format="pdf")
+
+        plt.show()
+
+    if args.distance == 'l2':
+        dist_fn = l2
+    elif args.distance == 'cos':
+        dist_fn = cos
 
     within_vars = []
     spk2num_utt = []
     for spk in spk2vec:
-        within_vars.append(np.var(spk2vec[spk], axis=0).sum())
+        spk_mean = np.mean(spk2vec[spk], axis=0)
+        within_var = 0
+        for vec in spk2vec[spk]:
+            within_var += dist_fn(vec, spk_mean) ** 2
+        within_var /= len(spk2vec[spk])
+
+        # within_var = np.var(spk2vec[spk], axis=0).sum()
+        within_vars.append(within_var)
         spk2num_utt.append(len(spk2vec[spk]))
 
     within_var = np.array(within_vars) * np.array(spk2num_utt)
@@ -105,7 +130,9 @@ if __name__ == '__main__':
     overall_mean = np.mean(all_vectors, axis=0)
     between_var = 0
     for spk in spk2vec:
-        between_var += np.sum((np.mean(spk2vec[spk], axis=0) - overall_mean) ** 2) * len(spk2vec[spk])
+        # between_var += np.sum((np.mean(spk2vec[spk], axis=0)-overall_mean)**2) * len(spk2vec[spk])
+        between_var += dist_fn(np.mean(spk2vec[spk], axis=0), overall_mean) ** 2 * len(spk2vec[spk])
+
     between_var /= np.sum(spk2num_utt)
 
     print("Variance Within-Class Between-Class\n", '         {:>7.4f}    {:>7.4f}'.format(within_var, between_var))
