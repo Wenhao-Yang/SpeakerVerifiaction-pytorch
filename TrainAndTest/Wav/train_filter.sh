6d1f7f19d4942790eef38d1bcfcade953d8c9bb1
Mac-Yang <874681044@qq.com>
2022-10-23 10:00:52 +0800

    [data] vox1 wave

--- TrainAndTest/Wav/train_filter.sh
+++ TrainAndTest/Wav/train_filter.sh
@@ -415,7 +415,7 @@ if [ $stage -le 21 ]; then
   # _lrr${lr_ratio}_lsr${loss_ratio}
   for lamda_beta in 2.0;do
     for seed in 123456 ; do
-    for type in 1 01 2 ;do
+    for type in 1234 ;do
 #    for type in 012 02 023 67 123 234 345 456 567 7 ;do
       #
      feat=fb${input_dim}
@@ -423,7 +423,7 @@ if [ $stage -le 21 ]; then
     #   CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py
     #   CUDA_VISIBLE_DEVICES=3,5 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417410 --nnodes=1 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/aidata_resnet.yaml --seed=${seed}
     #   CUDA_VISIBLE_DEVICES=5,6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417430 --nnodes=1 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Wav/vox1_resnet.yaml --seed=${seed}
-     CUDA_VISIBLE_DEVICES=4,5 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417420 --nnodes=1 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Wav/mani_layers/vox1_resnet_mixup_${type}.yaml --seed=${seed} --lamda-beta ${lamda_beta}
+     CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417420 --nnodes=1 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Wav/mani_layers/vox1_resnet_mixup_${type}.yaml --seed=${seed} --lamda-beta ${lamda_beta}
     done
     done
   done
