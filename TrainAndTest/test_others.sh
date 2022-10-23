3d990bbfcef94e6ab3c2cdca6bcc2f3f167cff78
Mac-Yang <874681044@qq.com>
2022-10-23 21:45:43 +0800

    [data] vox1 wave 1234

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -2376,7 +2376,7 @@ if [ $stage -le 201 ]; then
 
   for lamda_beta in 2.0 ;do
   for mani in 1234;do
-  for seed in 123457 ;do
+  for seed in 123456 ;do
     model_dir=ThinResNet34/vox1/wave_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_var2ses_bashuf2_dist_mani${mani}_lamda${lamda_beta}/${seed}
     python -W ignore TrainAndTest/test_egs.py \
       --model ${model} --resnet-size ${resnet_size} \
