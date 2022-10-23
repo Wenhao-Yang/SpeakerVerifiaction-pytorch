b9dcf371d56f77c5dbfd21bc63a0eb3d7a560eae
Mac-Yang <874681044@qq.com>
2022-10-23 18:19:10 +0800

    [data] vox1 wave 1234

--- Eval/plt_det.sh
+++ Eval/plt_det.sh
@@ -6,7 +6,7 @@
 # time: 2021/9/1 15:46
 # Description:
 
-stage=0
+stage=2
 if [ $stage -le 0 ]; then
   python Eval/plt_det.py --score-name 英语,汉语 \
     --score-file Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var/score.2021.09.01.16\:24\:29,Data/xvector/TDNN_v5/aishell2/spect_egs_baseline/arcsoft_0ce/inputMean_STAP_em512_wde4/aishell2_test_epoch_60_var/score.2021.09.01.16:35:39 \
@@ -21,3 +21,13 @@ if [ $stage -le 1 ]; then
     --save-path Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/cnceleb_test_epoch_50_var \
     --pf-max 0.1
 fi
+
+if [ $stage -le 2 ]; then
+  python Eval/plt_tsne.py --scp-file Data/xvector/ThinResNet34/vox1/wave_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_var2ses_bashuf2_dist/123456/vox1_test_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/baseline.v1.pdf
+
+  python Eval/plt_tsne.py --scp-file Data/xvector/ThinResNet34/vox1/wave_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_var2ses_bashuf2_dist_mani0_lamda2.0/123456/vox1_test_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/manifold0.v1.pdf
+
+  python Eval/plt_tsne.py --scp-file Data/xvector/ThinResNet34/vox1/wave_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_var2ses_bashuf2_dist_mani023_lamda2.0/123456/vox1_test_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/manifold023.v1.pdf
+
+fi
+
--- Eval/plt_tsne.py
+++ Eval/plt_tsne.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
         assert len(this_points)>0, 'start:stop is %s:%s' %(start, stop)
         emb_group.append(this_points)
 
-    plt.figure(figsize=(8, 9))
+    plt.figure(figsize=(8, 8))
     leng = []
     for idx, group in enumerate(emb_group):
         if len(group) > 0:
@@ -81,7 +81,9 @@ if __name__ == '__main__':
             leng.append(spks_this[idx])
             plt.scatter(group[:, 0], group[:, 1], color=c, s=10)
 
-    plt.legend(leng, loc="best")
+    plt.legend(leng, loc="best", fontsize=18)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
     if args.out_pdf.endswith('pdf'):
         plt.savefig(args.out_pdf, format="pdf")
 
