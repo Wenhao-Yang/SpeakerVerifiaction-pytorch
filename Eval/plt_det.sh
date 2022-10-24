f8cfcfce08d8c892c3908c9b2ca88869c2f325c1
Mac-Yang <874681044@qq.com>
2022-10-24 22:44:02 +0800

    [data] vox1 wave 1234

--- Eval/plt_det.sh
+++ Eval/plt_det.sh
@@ -8,7 +8,7 @@
 
 lstm_dir=/home/yangwenhao/project/lstm_speaker_verification
 
-stage=3
+stage=2
 if [ $stage -le 0 ]; then
   python Eval/plt_det.py --score-name 英语,汉语 \
     --score-file Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var/score.2021.09.01.16\:24\:29,Data/xvector/TDNN_v5/aishell2/spect_egs_baseline/arcsoft_0ce/inputMean_STAP_em512_wde4/aishell2_test_epoch_60_var/score.2021.09.01.16:35:39 \
@@ -33,19 +33,19 @@ if [ $stage -le 2 ]; then
   model_dir=Data/xvector/ThinResNet34/vox1/wave_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_var2ses_bashuf2_dist
 
   python -W ignore Eval/plt_tsne.py --scp-file ${model_dir}/123456/${dataset}_${subset}_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/baseline.pdf \
-    --hard-vector ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/hard_vectors \
+    --hard-vector ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/hard_vectors_2 \
     --num-spk ${num_spk} --distance ${distance} \
     --pca-dim 64
 
-  python -W ignore Eval/plt_tsne.py --scp-file ${model_dir}_mani0_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/input.pdf \
-    --num-spk ${num_spk} --distance ${distance} \
-    --pca-dim 64 \
-    --hard-vector ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/hard_vectors
-#
-  python -W ignore Eval/plt_tsne.py --scp-file ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/manifold.pdf \
-    --num-spk ${num_spk} --distance ${distance} \
-    --pca-dim 64 \
-    --hard-vector ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/hard_vectors
+#  python -W ignore Eval/plt_tsne.py --scp-file ${model_dir}_mani0_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/input.pdf \
+#    --num-spk ${num_spk} --distance ${distance} \
+#    --pca-dim 64 \
+#    --hard-vector ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/hard_vectors_2
+##
+#  python -W ignore Eval/plt_tsne.py --scp-file ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/manifold.pdf \
+#    --num-spk ${num_spk} --distance ${distance} \
+#    --pca-dim 64 \
+#    --hard-vector ${model_dir}_mani023_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/hard_vectors_2
   exit
 fi
 
