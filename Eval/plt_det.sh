19f1278049521a5a15f667664415afe1ffb6d648
Mac-Yang <874681044@qq.com>
2022-10-24 19:11:10 +0800

    [data] vox1 wave 1234

--- Eval/plt_det.sh
+++ Eval/plt_det.sh
@@ -8,7 +8,7 @@
 
 lstm_dir=/home/yangwenhao/project/lstm_speaker_verification
 
-stage=2
+stage=3
 if [ $stage -le 0 ]; then
   python Eval/plt_det.py --score-name 英语,汉语 \
     --score-file Data/xvector/TDNN_v5/vox2/pyfb_egs_baseline/arcsoft/featfb40_ws25_inputMean_STAP_em512_wde4_var/vox1_test_epoch_50_var/score.2021.09.01.16\:24\:29,Data/xvector/TDNN_v5/aishell2/spect_egs_baseline/arcsoft_0ce/inputMean_STAP_em512_wde4/aishell2_test_epoch_60_var/score.2021.09.01.16:35:39 \
@@ -50,7 +50,7 @@ if [ $stage -le 3 ]; then
     --trials ${lstm_dir}/data/vox1/test/trials \
     --threshold 0.2446 \
     --output-file Data/xvector/ThinResNet34/vox1/wave_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_var2ses_bashuf2_dist/123456/${dataset}_${subset}_var/testwidth1.000000/hard_vectors \
-    --confidence-interval 0.05
+    --confidence-interval 0.0
 
 #  python -W ignore Eval/plt_tsne.py --scp-file Data/xvector/ThinResNet34/vox1/wave_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_var2ses_bashuf2_dist_mani0_lamda2.0/123456/${dataset}_${subset}_var/testwidth1.000000/xvectors.scp --out-pdf Misc/data/manifold0.v1.pdf \
 #    --num-spk ${num_spk} --distance ${distance}
