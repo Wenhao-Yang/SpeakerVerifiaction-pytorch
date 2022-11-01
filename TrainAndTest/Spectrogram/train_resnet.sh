c9f56e479f925a84683de9a8682e4f4cda9f1f1e
Mac-Yang <874681044@qq.com>
2022-11-01 10:35:01 +0800

    [train] vox2 klsp dist resnet34
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/Spectrogram/train_resnet.sh
+++ TrainAndTest/Spectrogram/train_resnet.sh
@@ -676,7 +676,7 @@ if [ $stage -le 61 ]; then
   # _lrr${lr_ratio}_lsr${loss_ratio}
  for seed in 123456 ; do
    echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
-   CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Spectrogram/ResNets/vox2_resnet.yaml --seed=${seed}
+   CUDA_VISIBLE_DEVICES=4,5 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417420 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Spectrogram/ResNets/vox2_resnet.yaml --seed=${seed}
 
 #    CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Fbank/ResNets/aidata_resnet_mixup.yaml --seed=${seed}
   done
