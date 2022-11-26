7dc967b7dfd5d72d6d3a8c88e077f527f8cb199b
Mac-Yang <874681044@qq.com>
2022-11-26 09:53:14 +0800

    [test] v1 nj8 resnet10 fb80
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -2696,9 +2696,10 @@ if [ $stage -le 301 ]; then
 #--score-norm as-norm --n-train-snts 100000 --cohort-size 5000 \
 #     --vad-select \
 
+echo -e "\n\033[1;4;31m Stage${stage}: Test ${model}${resnet_size} in ${test_set}_egs with ${loss} with ${input_norm} normalization \033[0m\n"
+
 for seed in 123456 123457 123458; do
-  for s in all; do
-    echo -e "\n\033[1;4;31m Stage${stage}: Test ${model}${resnet_size} in ${test_set}_egs with ${loss} with ${input_norm} normalization \033[0m\n"
+  for s in all; do    
 #    model_dir=ThinResNet34/cnceleb/klfb40_egs12_both_binary/arcsoft_sgd_rop/Mean_batch256_basic_downk3_none1_SAP2_dp01_alpha0_em512_dom1_wd5e4_var_es
     # model_dir=ThinResNet34/cnceleb/klfb_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_vares_bashuf2_dist_mani234_lamda2.0/123456
     # model_dir=ThinResNet34/vox1/klfb80_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_SAP2_em256_dp01_alpha0_none1_wde4_varesmix2_bashuf2/${seed}
