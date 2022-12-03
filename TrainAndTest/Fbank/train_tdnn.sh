61ab38cdcf43d7920190c69b846ff06119930ad8
Mac-Yang <874681044@qq.com>
2022-12-03 17:34:51 +0800

    [train] v2 ecapa fb40 baseline cyclic
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/Fbank/train_tdnn.sh
+++ TrainAndTest/Fbank/train_tdnn.sh
@@ -1757,7 +1757,7 @@ if [ $stage -le 300 ]; then
 #   CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py
   CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/TDNNs/vox2_ecapa.yaml --seed=${seed}
 
-  #  CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 --master_port=417420 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Fbank/TDNNs/vox2_ecapa_mixup.yaml --lamda-beta 0.2
+   CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 --master_port=417420 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Fbank/TDNNs/vox2_ecapa_mixup.yaml --lamda-beta 0.2
 
 #   CUDA_VISIBLE_DEVICES=2,4 python -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Fbank/TDNNs/vox2_tdnn_mixup.yaml --seed=${seed}
   done
