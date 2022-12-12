2ca049fc1be40ecbb3dcf8a9e3e788e4518c0e8d
Mac-Yang <874681044@qq.com>
2022-12-12 10:00:36 +0800

    [misc] weights eer
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/train_egs.py
+++ TrainAndTest/train_egs.py
@@ -575,6 +575,7 @@ def select_samples(train_loader, model, ce, select_score='loss'):
                     rest_dataset.append(train_dataset[int(j)])
 
     dataset = np.array(dataset)
+    dataset = dataset[:-(len(dataset)%args.batch_size)]
     dataset = dataset[np.argsort(dataset, axis=0)[:, 2]]
 
     np.random.shuffle(dataset)
