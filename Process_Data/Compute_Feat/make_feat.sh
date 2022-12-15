136551c522d5cb56f75865090d59bd186cc358ac
Mac-Yang <874681044@qq.com>
2022-12-15 12:24:40 +0800

    [train] fb40 aidata instance
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -47,54 +47,57 @@ fi
 #exit
 #stage=1000
 if [ $stage -le 1 ]; then
-  dataset=vox2
+  # dataset=vox2
 #  dataset=cnceleb
 #  dataset=aishell2
-#  dataset=aidata
+ dataset=aidata
 
   #  feat_type=pyfb
 #  dataset=vox1
- feat=wave
- feat_format=wav
- feat_type=wav
+#  feat=wave
+#  feat_format=wav
+#  feat_type=wav
 
   # feat=klsp
   # feat_type=klsp
-  # feat_format=kaldi
+
+  feat=klfb
+  feat_type=klfb
+  feat_format=kaldi
 
 #  num_frames=400
-  num_frames=32000 input_per_spks=896
+  num_frames=400 input_per_spks=896
   # num_frames=400 input_per_spks=896
 #        --remove-vad \
 #--domain \
+
+  sample_type=instance
 
   echo -e "\n\033[1;4;31m Stage ${stage}: making ${feat} egs for ${dataset}\033[0m\n"
   #  for s in dev_log dev_aug_1m_log ; do
 #  for s in dev_fb24 dev_fb40 dev_f64 dev_fb80; do
 #/${feat}
-  for s in dev ; do #${feat}
+  for s in train_fb40 ; do #${feat}
     python Process_Data/Compute_Feat/make_egs.py \
       --data-dir ${lstm_dir}/data/${dataset}/${s} \
       --out-dir ${lstm_dir}/data/${dataset}/egs/${feat} \
-      --nj 12 \
-      --feat-type ${feat_type} \
+      --nj 12 --feat-type ${feat_type} \
       --train \
-      --input-per-spks ${input_per_spks} --num-frames ${num_frames} \
+      --input-per-spks ${input_per_spks} --num-frames ${num_frames} --sample-type ${sample_type} \
       --feat-format ${feat_format} \
       --out-format kaldi_cmp \
       --num-valid 2 \
-      --out-set ${s}
+      --out-set ${s}_inst
 
     python Process_Data/Compute_Feat/make_egs.py \
       --data-dir ${lstm_dir}/data/${dataset}/${s} \
       --out-dir ${lstm_dir}/data/${dataset}/egs/${feat} \
-      --nj 12 \
-      --feat-type ${feat_type} \
-      --num-frames ${num_frames} --input-per-spks ${input_per_spks} \
+      --nj 12 --feat-type ${feat_type} \
+      --num-frames ${num_frames} --input-per-spks ${input_per_spks} --sample-type ${sample_type} \
       --feat-format ${feat_format} \
       --out-format kaldi_cmp \
       --num-valid 2 \
-      --out-set ${s}_valid
+      --out-set ${s}_valid_inst
   done
   exit
 fi
