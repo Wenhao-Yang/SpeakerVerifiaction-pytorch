78c72e1b49d76a132061e3b5fe99006bdb3fb99a
Mac-Yang <874681044@qq.com>
2022-12-16 19:23:42 +0800

    [train] v1 klsp baseline resnet34
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/Spectrogram/train_resnet.sh
+++ TrainAndTest/Spectrogram/train_resnet.sh
@@ -653,7 +653,7 @@ if [ $stage -le 62 ]; then
    echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
     # CUDA_VISIBLE_DEVICES=1,1 OMP_NUM_THREADS=6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417425 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Spectrogram/ResNets/vox1_resnet18.yaml --seed=${seed}
 
-    CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417425 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Spectrogram/ResNets/vox1_resnet34.yaml --seed=${seed}
+    CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=6 python -m torch.distributed.launch --nproc_per_node=2 --master_port=417423 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Spectrogram/ResNets/vox1_resnet34.yaml --seed=${seed}
 
 #    CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Fbank/ResNets/aidata_resnet_mixup.yaml --seed=${seed}
   done
