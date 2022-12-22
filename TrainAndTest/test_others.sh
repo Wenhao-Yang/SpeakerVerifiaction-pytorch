d5d2d7571028addaf7bf4dc5ab30cb4ffb48d280
Mac-Yang <874681044@qq.com>
2022-12-22 15:28:43 +0800

    [test] drop mel 0.5
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -2019,9 +2019,10 @@ if [ $stage -le 200 ]; then
   sname=dev #_aug_com
 
   weight=v2_rclean_gean #v2_rclean_gax #mel
-  weight_norm=mean scale=0.5 weight_p=0.0
+  weight_norm=max scale=0.5 weight_p=0.0
   echo -e "\n\033[1;4;31mStage ${stage}: Testing ${model}_${resnet_size} in ${datasets} with ${loss} kernel 5,5 \033[0m\n"
 
+  for testset in vox1 magic;do
   for mask_layer in drop ; do
     for weight in mel; do
     for seed in 123456 123457 123458;do
@@ -2068,6 +2069,7 @@ if [ $stage -le 200 ]; then
       done
     done
     done
+  done
   done
   exit
 #+-------------------+-------------+-------------+-------------+--------------+-------------------+
