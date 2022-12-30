61cd97dbeb13ce155d359dfe167ba2b165f83b12
Mac-Yang <874681044@qq.com>
2022-12-30 18:22:14 +0800

    [test] cnc vectors
    Signed-off-by: Mac-Yang <874681044@qq.com>

---
+++ Extraction/compute_dist.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@Author: yangwenhao
+@Contact: 874681044@qq.com
+@Software: VS Code
+@File: compute_dist.py
+@Time: 2022/12/30 11:32
+@Overview: 
+'''
+
+import argparse
+import torch
+import kaldiio
+
+parser = argparse.ArgumentParser(
+    description='Extract probilities for x-vector during training.')
+parser.add_argument('--data-dir', type=str, help='path to dataset')
+parser.add_argument('--checkpoint', type=str, help='path to dataset')
+
+args = parser.parse_args()
+
+ckp = torch.load(args.checkpoint)
+model_dict = ckp['state_dict']
+classifier_centers = model_dict['classifier.W']  # torch.Size([256, 797])
+classifier_centers = torch.nn.functional.normalize(classifier_centers, dim=0)
+classifier_centers = classifier_centers.cpu()
+
+vec_dict = kaldiio.load_scp(args.data_dir + '/xvectors.scp')
+sim_ark = args.data_dir + + '/sim.ark.gz'
+
+with kaldiio.WriteHelper('ark:| gzip -c > ' + sim_ark) as writer:
+
+    pbar = tqdm(vec_dict, ncols=100)
+    for k in pbar:
+        this_vec = torch.tensor(vec_dict[k]).reshape(1, -1)
+        this_vec = torch.nn.functional.normalize(this_vec, dim=1)
+        similarities = torch.matmul(this_vec, classifier_centers)
+
+        writer(str(k), similarities.squeeze().numpy())
---
+++ Extraction/sim.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+stage=0
+lstm_dir=/home/yangwenhao/project/lstm_speaker_verification
+
+# ===============================    LoResNet10    ===============================
+if [ $stage -le 0 ]; then
+  for epoch in 1 2 5 6 9 10 12 13 17 20 21 25 26 27 29 30 33 37 40 41; do
+    test_set=cnceleb subset=dev test_input=fix
+
+    model_dir=ThinResNet34/cnceleb/klfb_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_varesmix2_bashuf2_dist/123456
+    data_dir=Data/xvector/${model_dir}/${test_set}_${subset}_${epoch}_${test_input}/test
+    
+    python Extraction/compute_dist.py \
+        --data-dir ${data_dir} \
+        --checkpoint Data/checkpoint/${model_dir}/checkpoint_${epoch}.pth 
+  done
+fi
