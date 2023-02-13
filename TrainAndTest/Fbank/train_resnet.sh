4d6bee386c28fe456d7caa70383c3c689b88e8af
Mac-Yang <874681044@qq.com>
2023-02-13 21:10:23 +0800

    [misc] distance
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -890,7 +890,7 @@ if [ $stage -le 103 ]; then
   seed=123457
   # CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/cnc1_resnet_simple_domain.yaml --seed=${seed}
 
- for seed in 123456 ; do
+ for seed in 123457 123458 ; do
     echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
     # CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/cnc1_resnet_simple.yaml --seed=${seed}
 
