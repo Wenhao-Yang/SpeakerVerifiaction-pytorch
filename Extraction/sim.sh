dce2b670b08064b16c3866cb73c86e24c4d67e46
Mac-Yang <874681044@qq.com>
2023-02-17 13:40:30 +0800

    [extract] mean no
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- Extraction/sim.sh
+++ Extraction/sim.sh
@@ -6,8 +6,8 @@ lstm_dir=/home/yangwenhao/project/lstm_speaker_verification
 # ===============================    ResNet34    ===============================
 if [ $stage -le 0 ]; then
   
-  # for epoch in 10; do
-  for ((epoch=1; epoch<=46; epoch=epoch+1)); do
+  for epoch in 46; do
+  # for ((epoch=1; epoch<=46; epoch=epoch+1)); do
     
     # model_dir=ThinResNet34/cnceleb/klfb_egs_baseline/arcsoft_sgd_rop/Mean_batch256_seblock_red2_downk1_avg5_ASTP2_em256_dp01_alpha0_none1_wde4_varesmix2_bashuf2_dist/123456
     test_set=cnceleb subset=dev test_input=fix
@@ -15,20 +15,20 @@ if [ $stage -le 0 ]; then
 
     # model_dir=ThinResNet34/vox1/klfb_egs_baseline/arcsoft_sgd_step/Mean_batch256_seblock_red2_downk1_avg5_SAP2_em256_dp01_alpha0_none1_wd5e4_varesmix2_bashuf2_dist_cnc_core/percent0.5_random/123456
 
-    data_dir=Data/xvector/${model_dir}/${test_set}_${subset}_${epoch}_${test_input}/test
+    data_dir=Data/xvector/${model_dir}/${test_set}_${subset}_${epoch}_${test_input}_nomean/test
     python Extraction/compute_dist.py \
         --data-dir ${data_dir} \
         --checkpoint Data/checkpoint/${model_dir}/checkpoint_${epoch}.pth \
-        --gpu-id 2
+        --gpu-id 5
 
-    test_set=vox1 subset=dev test_input=fix
-    model_dir=ThinResNet34/vox1/klfb_egs_baseline/arcsoft_sgd_step/Mean_batch256_seblock_red2_downk1_avg5_SAP2_em256_dp01_alpha0_none1_wd5e4_varesmix2_bashuf2_dist_cnc_core/percent0.5_random/123456
+    # test_set=vox1 subset=dev test_input=fix
+    # model_dir=ThinResNet34/vox1/klfb_egs_baseline/arcsoft_sgd_step/Mean_batch256_seblock_red2_downk1_avg5_SAP2_em256_dp01_alpha0_none1_wd5e4_varesmix2_bashuf2_dist_cnc_core/percent0.5_random/123456
 
-    data_dir=Data/xvector/${model_dir}/${test_set}_${subset}_${epoch}_${test_input}/test
-    python Extraction/compute_dist.py \
-        --data-dir ${data_dir} \
-        --checkpoint Data/checkpoint/${model_dir}/checkpoint_${epoch}.pth \
-        --gpu-id 2
+    # data_dir=Data/xvector/${model_dir}/${test_set}_${subset}_${epoch}_${test_input}/test
+    # python Extraction/compute_dist.py \
+    #     --data-dir ${data_dir} \
+    #     --checkpoint Data/checkpoint/${model_dir}/checkpoint_${epoch}.pth \
+    #     --gpu-id 2
 
 
   done
