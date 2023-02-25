a2b85082126d6853261b31ae9873f4a5b2f581dc
Mac-Yang <874681044@qq.com>
2023-02-25 12:20:31 +0800

    [misc]
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- Light/model.py
+++ Light/model.py
@@ -209,6 +209,7 @@ class SpeakerModule(LightningModule):
         return loss
 
     def on_train_epoch_end(self, outputs) -> None:
+        pdb.set_trace()
         print(self.current_epoch)
         self.print("Epoch {:>2d} Loss: {:>7.4f} Accuracy: {:>6.2f}%".format(
             self.current_epoch, torch.cat(outputs).mean()), np.mean(self.train_accuracy))
