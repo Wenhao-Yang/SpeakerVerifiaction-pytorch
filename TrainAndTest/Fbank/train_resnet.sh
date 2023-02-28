cbcb79a39f7510af49a4514539a5b33f62a92471
Mac-Yang <874681044@qq.com>
2023-02-28 22:04:09 +0800

    [train] cls aug
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -2,7 +2,7 @@
 
 stage=44
 waited=0
-while [ `ps 1412981 | wc -l` -eq 2 ]; do
+while [ `ps 4088052 | wc -l` -eq 2 ]; do
   sleep 60
   waited=$(expr $waited + 1)
   echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
