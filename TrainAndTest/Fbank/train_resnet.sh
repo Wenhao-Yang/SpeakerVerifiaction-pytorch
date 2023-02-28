ad0dec0fa367082b30008ac0a0030b2fccfb598a
Mac-Yang <874681044@qq.com>
2023-02-28 22:01:34 +0800

    [train] cls aug
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=103
+stage=44
 waited=0
 while [ `ps 1412981 | wc -l` -eq 2 ]; do
   sleep 60
@@ -563,8 +563,9 @@ if [ $stage -le 44 ]; then
   # CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/vox1_aug/vox1_clsaug5.yaml --seed=${seed}
 
   # seed=123456
+  for lamda_beta in 2.0 1.0 ; do
   for seed in 123456 123457 123458 ; do
-   for layer in 8 7 6 5  ; do
+   for layer in 4 3 2 1  ; do
    echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
   #  CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist_mixup.py --train-config=TrainAndTest/Fbank/ResNets/cnc1_resnet_mixup.yaml --seed=${seed}
 
@@ -576,8 +577,9 @@ if [ $stage -le 44 ]; then
   # CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/vox1_aug/vox1_clsaug5.yaml --seed=${seed}
 
   sleep 5
-  CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs/train_egs_dist_clsaug.py --train-config=TrainAndTest/Fbank/ResNets/vox1_aug/vox1_clsaug${layer}.yaml --seed=${seed} --lamda-beta 1.0 
+  CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs/train_egs_dist_clsaug.py --train-config=TrainAndTest/Fbank/ResNets/vox1_aug/vox1_clsaug${layer}.yaml --seed=${seed} --lamda-beta ${lamda_beta} 
 
+  done
   done
   done
   exit
