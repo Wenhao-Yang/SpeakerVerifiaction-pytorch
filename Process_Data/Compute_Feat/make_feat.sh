bd9d1572c446fcc2fd09413c739fb28b00f17ccf
Wenhao@Server2Win <874681044@qq.com>
2023-03-01 12:55:00 +0800

    [misc] aug

--- Process_Data/Compute_Feat/make_feat.sh
+++ Process_Data/Compute_Feat/make_feat.sh
@@ -66,7 +66,7 @@ if [ $stage -le 1 ]; then
 
   echo -e "\n\033[1;4;31m Stage ${stage}: making ${feat} egs for ${dataset}\033[0m\n"
   #  for s in dev_log dev_aug_1m_log ; do
-  dataset=vox2
+  dataset=vox1
   for subset_str in vox1; do
     # subset_str=
     # if [[ $dataset == vox1 ]];then
