d4e7dcfc6c79d83048d695ac6154c2fa6fc104cd
Mac-Yang <874681044@qq.com>
2023-03-03 17:41:27 +0800

    [misc] results
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/Fbank/train_resnet.sh
+++ TrainAndTest/Fbank/train_resnet.sh
@@ -563,7 +563,7 @@ if [ $stage -le 44 ]; then
   # CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs_dist.py --train-config=TrainAndTest/Fbank/ResNets/vox1_aug/vox1_clsaug5.yaml --seed=${seed}
 
   # seed=123456
-  for lamda_beta in 2.0 ; do
+  for lamda_beta in 0.2 ; do
   for seed in 123456 123457 123458 ; do
    for layer in 7 5 3 2 ; do
    echo -e "\n\033[1;4;31m Stage ${stage}: Training ${model}_${encod} in ${datasets}_${feat} with ${loss}\033[0m\n"
