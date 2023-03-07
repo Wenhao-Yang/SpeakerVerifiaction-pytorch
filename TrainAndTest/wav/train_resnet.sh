4bfcc1c4fd579ed15ad76657794aa9171ec41dcd
Wenhao-Yang A315 <874681044@qq.com>
2023-03-07 18:21:52 +0800

    [feat] int float
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/wav/train_resnet.sh
+++ TrainAndTest/wav/train_resnet.sh
@@ -35,7 +35,7 @@ if [ $stage -le 0 ]; then
     # sleep 5
     # CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs/train_egs_dist.py --train-config=TrainAndTest/wav/resnet/aidata_int.yaml --seed=${seed}
 
-    CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/aidata_float_original.yaml --seed=${seed}
+    CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/aidata_int_original.yaml --seed=${seed}
 
 
   done
