2c0579b8e863e7f872220be4fd68a79cb6c11ba2
Wenhao-Yang A315 <874681044@qq.com>
2023-03-08 21:52:45 +0800

    [train] format

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -2683,7 +2683,7 @@ if [ $stage -le 301 ]; then
   train_set=vox1 test_set=vox1
   train_subset=
 #  subset=dev
-  subset=dev test_input=var
+  subset=test test_input=var
   epoch=13
 
 #     --trials subtrials/trials_${s} --score-suffix ${s} \
