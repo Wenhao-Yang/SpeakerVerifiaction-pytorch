6b70efe4c3d97d982c5ddb0f7e6c3a4d002a4a7d
Wenhao-Yang A315 <874681044@qq.com>
2023-03-13 21:35:55 +0800

    [train] v1 centers
    Signed-off-by: Mac-Yang <874681044@qq.com>

--- TrainAndTest/train_egs/train_egs.py
+++ TrainAndTest/train_egs/train_egs.py
@@ -280,7 +280,7 @@ def select_samples(train_dir, train_loader, model, args, select_score='loss'):
     for i in range(train_dir.num_spks):
         score_dict[i] = []
 
-    train_dir.return_idx = True
+    train_loader.dataset.return_idx = True
     if len(train_dir.rest_dataset) > 0:
         train_dir.dataset = np.concatenate(
             [train_dir.dataset, train_dir.rest_dataset])
