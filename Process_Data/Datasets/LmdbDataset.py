7b18137155d32e077e16799eef7c82787f3af29d
Wenhao-Yang A315 <874681044@qq.com>
2023-03-13 11:36:42 +0800

    [train] v2 hdf5 mean

--- Process_Data/Datasets/LmdbDataset.py
+++ Process_Data/Datasets/LmdbDataset.py
@@ -1101,6 +1101,7 @@ class Hdf5TrainDataset(Dataset):
         # utt2num_frames = dir + '/utt2num_frames'
         utt2num_frames = dir + '/utt2num_frames' if feat_type != 'wav' else dir + '/utt2dur'
         hdf5_file = dir + '/feat.h5py'
+        self.hdf5_file = hdf5_file
         self.sample_type = sample_type
         self.segment_len = segment_len
         assert os.path.exists(hdf5_file)
@@ -1210,7 +1211,7 @@ class Hdf5TrainDataset(Dataset):
 
         self.samples_per_speaker = int(samples_per_speaker)
         self.all_utts = list(utt2spk_dict.keys())
-        self.reader = h5py.File(hdf5_file, 'r')
+        # self.reader = h5py.File(hdf5_file, 'r')
         self.speakers = speakers
         self.dataset = dataset
 
@@ -1230,7 +1231,8 @@ class Hdf5TrainDataset(Dataset):
             while True:
                 (uid, start, end) = self.base_utts[sid]
                 # pdb.set_trace()
-                y = self.loader(self.reader, uid, start=start, stop=end)
+                reader = h5py.File(self.hdf5_file, 'r')
+                y = self.loader(reader, uid, start=start, stop=end)
                 y = y[start:end]
 
                 sid = self.utt2spk_dict[uid]
@@ -1251,7 +1253,10 @@ class Hdf5TrainDataset(Dataset):
             start = 0 if self.utt2num_frames[uid] <= self.segment_len else np.random.randint(
                 0, self.utt2num_frames[uid] - self.segment_len)
             end = start + self.segment_len
-            y = self.loader(self.reader, uid, start=start, stop=end)
+
+            reader = h5py.File(self.hdf5_file, 'r')
+            y = self.loader(reader, uid, start=start, stop=end)
+            # y = self.loader(self.reader, uid, start=start, stop=end)
             # y = np.concatenate((y, feature), axis=self.c_axis)
 
         feature = self.transform(y.reshape(1,-1))
