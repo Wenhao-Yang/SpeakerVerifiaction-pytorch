329f26c5f16fcd4c84a7dcdfa4df411b0a5e12b5
Wenhao-Yang A315 <874681044@qq.com>
2023-03-18 12:24:12 +0800

    [test] ecapa brain

--- Light/dataset.py
+++ Light/dataset.py
@@ -207,7 +207,7 @@ def Sampler_Loaders(train_dir, valid_dir, train_extract_dir, config_args):
 
     min_chunk_size = int(config_args['random_chunk'][0])
     max_chunk_size = int(config_args['random_chunk'][1])
-    pad_dim = 2 if config_args['feat_format'] == 'kaldi' and 'trans_fbank' not in config_args else 3
+    pad_dim = 2 if config_args['feat_format'] == 'kaldi' or 'trans_fbank' in config_args else 3
 
     # if 'coreset_percent' in config_args and config_args['coreset_percent'] > 0:
     #     train_length = len(train_dir)
