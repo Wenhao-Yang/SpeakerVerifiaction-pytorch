1ab02fb925745b75c25cd47b15f0b1ccc7ec9607
Wenhao-Yang A315 <874681044@qq.com>
2023-03-27 13:18:04 +0800

    [train] ecapa brain light

--- Light/model.py
+++ Light/model.py
@@ -222,7 +222,7 @@ class SpeakerModule(LightningModule):
 
         # start = time.time()
         data, label = batch
-        print(data.shape[0])
+        print(data.shape)
         logits, embeddings = self.encoder(data)
         loss, other_loss = self.loss(logits, embeddings, label)
 
