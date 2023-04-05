def2388e799789bcce32a39fc4a33acbe4531546
Wenhao-Yang A315 <874681044@qq.com>
2023-04-05 21:29:17 +0800

    [train] hdf5 cnc1 speed 3 ways

--- Define_Model/FilterLayer.py
+++ Define_Model/FilterLayer.py
@@ -301,6 +301,7 @@ class SparseFbankLayer(nn.Module):
         self.SpareFbank.data = (weight/ weight.norm(p=2, dim=0).reshape(1,-1)).abs()
 
         output = torch.transpose(specgram, 1, 2)
+        print(output.shape, self.SpareFbank.shape)
         output = torch.matmul(output, self.SpareFbank)
         
         return torch.log(output + 1e-6)
