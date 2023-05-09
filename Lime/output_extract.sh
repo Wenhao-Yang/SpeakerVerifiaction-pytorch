48df618a86dc42935354134a2837f202f81a2bbd
Wenhao-Yang A315 <874681044@qq.com>
2023-05-09 12:11:20 +0800

    [misc] vis gfa trl

--- Lime/output_extract.sh
+++ Lime/output_extract.sh
@@ -3,7 +3,7 @@
 stage=301
 waited=0
 lstm_dir=/home/work2020/yangwenhao/project/lstm_speaker_verification
-while [ $(ps 438120 | wc -l) -eq 2 ]; do
+while [ $(ps 827849 | wc -l) -eq 2 ]; do
   sleep 60
   waited=$(expr $waited + 1)
   echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
