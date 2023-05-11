68793d71df7b92f0583b95652920d0919cd062ad
Wenhao-Yang A315 <874681044@qq.com>
2023-05-11 22:15:29 +0800

    [misc] insert del

--- Lime/output_extract.sh
+++ Lime/output_extract.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-stage=301
+stage=300
 waited=0
 lstm_dir=/home/work2020/yangwenhao/project/lstm_speaker_verification
 while [ $(ps 827849 | wc -l) -eq 2 ]; do
@@ -898,7 +898,7 @@ if [ $stage -le 300 ]; then
   cam=gradient
   echo -e "\n\033[1;4;31m stage${stage} Training ${model}_${encoder_type} in ${train_set}_${test_set} with ${loss}\033[0m\n"
   
-  for cam in layer_cam ;do
+  for cam in gradient grad_cam grad_cam_pp fullgrad integrad ;do #layer_cam 
   # model_dir=${model}${resnet_size}/${train_set}/klfb_egs_baseline/arcsoft_sgd_rop/chn32_Mean_basic_downNone_none1_SAP2_dp01_alpha0_em256_wde4_var
     model_dir=ThinResNet34_ser06/Mean_batch256_basic_downk1_avg5_SAP2_em256_dp01_alpha0_none1_wde5_var/arcsoft_sgd_rop/vox2/123456
     epoch=41
@@ -942,8 +942,10 @@ if [ $stage -le 301 ]; then
   echo -e "\n\033[1;4;31m stage${stage} Training ${model}_${encoder_type} in ${train_set}_${test_set} with ${loss}\033[0m\n"
   
   for cam in layer_cam ;do
-  for pro_type in  del ; do
-  for threshold in 0.01 0.02 0.04 0.08 0.09; do
+  for pro_type in insert ; do
+  for ((i=1; i<=99; i=i+2)); do
+    threshold=`echo "$i 100" | awk '{printf("%0.2f\n",$1/$2)}'`
+  # for threshold in 0.01 0.02 0.04 0.08 0.09; do
   # model_dir=${model}${resnet_size}/${train_set}/klfb_egs_baseline/arcsoft_sgd_rop/chn32_Mean_basic_downNone_none1_SAP2_dp01_alpha0_em256_wde4_var
     model_dir=ThinResNet34_ser06/Mean_batch256_basic_downk1_avg5_SAP2_em256_dp01_alpha0_none1_wde5_var/arcsoft_sgd_rop/vox2/123456
     # Data/gradient/ThinResNet34_ser06/Mean_batch256_basic_downk1_avg5_SAP2_em256_dp01_alpha0_none1_wde5_var/arcsoft_sgd_rop/vox2/123456/epoch_41_var_layer_cam_soft/epoch_41
