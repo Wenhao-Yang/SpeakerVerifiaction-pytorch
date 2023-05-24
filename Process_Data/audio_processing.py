27f4f72986a92ad6659d59fd7dfcc2a030d2225a
Wenhao-Yang A315 <874681044@qq.com>
2023-05-24 19:00:31 +0800

    [lime] accuracy mask

--- Process_Data/audio_processing.py
+++ Process_Data/audio_processing.py
@@ -555,6 +555,7 @@ class CAMNormInput(object):
         data, grad = x
         if self.data_preprocess != None:
             # pdb.set_trace()
+            print(data.shape, grad.shape)
             data = torch.tensor(data).unsqueeze(1)
             data = self.data_preprocess(data).numpy()
             print(data.shape)
