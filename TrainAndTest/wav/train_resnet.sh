e2bb981392f48218fde3e7117da0f065db2a74eb
Wenhao-Yang A315 <874681044@qq.com>
2023-09-01 15:58:31 +0800

    [train] cnc baseline

--- TrainAndTest/wav/train_resnet.sh
+++ TrainAndTest/wav/train_resnet.sh
@@ -67,7 +67,7 @@ if [ $stage -le 0 ]; then
 
     # CUDA_VISIBLE_DEVICES=2,3 OMP_NUM_THREADS=12 torchrun --nproc_per_node=2 --master_port=41715 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/v2_fb/vox2_student_frl21.yaml --seed=${seed}
 
-    CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=12 torchrun --nproc_per_node=2 --master_port=41725 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/v2_fb/vox2_student_fgl7.yaml --seed=${seed}
+    # CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=12 torchrun --nproc_per_node=2 --master_port=41725 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/v2_fb/vox2_student_fgl7.yaml --seed=${seed}
 
     # CUDA_VISIBLE_DEVICES=2,3 OMP_NUM_THREADS=12 torchrun --nproc_per_node=2 --master_port=41715 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/v2_fb/vox2_student_fgl8.yaml --seed=${seed}
     # CUDA_VISIBLE_DEVICES=2,3 OMP_NUM_THREADS=12 torchrun --nproc_per_node=2 --master_port=41735 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/v2_fb/vox2_student_fgl4.yaml --seed=${seed}
@@ -93,7 +93,7 @@ if [ $stage -le 0 ]; then
     # CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=12 python -m torch.distributed.launch --nproc_per_node=2 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/resnet/vox1_int_frl_spect.yaml --seed=${seed}
     # python Light/main.py --config-yaml=TrainAndTest/wav/resnet/vox1_int_light.yaml --seed=${seed} --gpus=0,1
 
-
+    CUDA_VISIBLE_DEVICES=0,1 OMP_NUM_THREADS=12 torchrun --nproc_per_node=2 --master_port=41725 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/mani_layers/cnc1_resnet_baseline.yaml --seed=${seed}
   done
   done
   done
