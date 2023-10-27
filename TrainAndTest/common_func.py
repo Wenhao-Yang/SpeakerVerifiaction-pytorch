33cfaf380fed1a25ce6457e86d52ebdf124b32c6
wenhao-mac <874681044@qq.com>
2023-10-27 16:08:10 +0800

    [train] v1 baseline resnet spec

--- TrainAndTest/common_func.py
+++ TrainAndTest/common_func.py
@@ -398,6 +398,11 @@ def verification_extract(extract_loader, model, xvector_dir, epoch, test_input='
             # uid2vectors = []
             # for i in all_uid2vectors:
             #     uid2vectors.extend(i)
+            for uidvec in all_uid2vectors:
+                print(len(uidvec))
+                for u,v  in uidvec:
+                    print(v.shape, end=' ')
+
             uid2vectors = np.concatenate(all_uid2vectors)
             # uid2vectors = all_uid2vectors[0].extend(all_uid2vectors[1])
             # print('uid2vectors:', len(uid2vectors))
