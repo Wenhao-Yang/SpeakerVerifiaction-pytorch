65114f64e5e3fe8acfdea98821e18ebb4e8953df
wenhao-mac <874681044@qq.com>
2023-10-27 14:31:20 +0800

    [train] v1 baseline resnet spec

--- Define_Model/ResNet.py
+++ Define_Model/ResNet.py
@@ -252,6 +252,68 @@ class SEBottleneck(nn.Module):
         out = self.relu(out)
 
         return out
+    
+
+class SEBottleneck_v2(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(self, inplanes: int, planes: int, stride: int = 1, downsample: Optional[nn.Module] = None,
+                 groups: int = 1, base_width: int = 64, dilation: int = 1,
+                 norm_layer: Optional[Callable[..., nn.Module]] = None, reduction_ratio: int = 4, **kwargs) -> None:
+        super(SEBottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+
+        if stride == 1:
+            self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        elif stride == 2:
+            self.conv2 = conv5x5(width, width, stride, groups, dilation)
+
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+        self.reduction_ratio = reduction_ratio
+
+        # Squeeze-and-Excitation
+        self.se_layer = SqueezeExcitation(
+            inplanes=planes * self.expansion, reduction_ratio=reduction_ratio)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out = self.se_layer(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
 
 
 class SEBasicBlock_v2(nn.Module):
@@ -1017,7 +1079,7 @@ class ThinResNet(nn.Module):
             block = SEBasicBlock if resnet_size < 50 else SEBottleneck
             # block.reduction_ratio = red_ratio
         elif block_type == "seblock_v2":
-            block = SEBasicBlock_v2
+            block = SEBasicBlock_v2 if resnet_size < 50 else SEBottleneck_v2
         elif block_type == 'cbam':
             block = CBAMBlock
         elif block_type == 'cbam_v2':
