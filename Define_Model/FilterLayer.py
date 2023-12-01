fb035f7694d925ce2e32331ac27462efdf9ff417
Wenhao@Server2Win <874681044@qq.com>
2023-12-01 17:16:52 +0800

    [train] v1 ecapa fdrl sign for mask

--- Define_Model/FilterLayer.py
+++ Define_Model/FilterLayer.py
@@ -1134,6 +1134,42 @@ class FrequencyDecayReweightLayer(nn.Module):
         return "FrequencyDecayReweightLayer(input_dim=%d)" % (self.input_dim)
     
 
+class FrequencyDecayReweightLayer2(nn.Module):
+    def __init__(self, input_dim=161):
+        super(FrequencyDecayReweightLayer, self).__init__()
+        self.input_dim = input_dim
+        
+        self.decay_classifier = nn.Sequential(
+            nn.Linear(input_dim, input_dim),
+            nn.ReLU(),
+        )
+
+        self.decay_std = nn.Parameter(torch.ones(1, 1, 1, input_dim))
+        self.theta = 1.0
+        self.activation = nn.Sigmoid()
+
+    def forward(self, x):
+        """sumary_line
+        
+        Keyword arguments:
+        X -- batch, channel, time, frequency
+        Return: x + U
+        """
+        # assert self.weight.shape[-1] == x.shape[-1], print(self.weight.shape, x.shape)
+        freq_std   = x.std(dim=-2, keepdim=True)
+        freq_score = (freq_std - self.decay_std) / self.theta
+
+        sign = torch.sign(self.decay_classifier(freq_std))
+
+        f = 0.5 + self.activation(freq_score) * sign
+        # f = f 
+
+        return x * f
+
+    def __repr__(self):
+        return "FrequencyDecayReweightLayer2(input_dim=%d)" % (self.input_dim)
+    
+
 class FrequencyReweightLayer2(nn.Module):
     def __init__(self, input_dim=161):
         super(FrequencyReweightLayer2, self).__init__()
