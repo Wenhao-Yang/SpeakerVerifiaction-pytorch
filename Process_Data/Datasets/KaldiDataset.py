7118fe8d7255aa89330564870798a6c9e2b552e2
wenhao-mac <874681044@qq.com>
2024-02-29 17:49:13 +0800

    [train] v1 half

--- Process_Data/Datasets/KaldiDataset.py
+++ Process_Data/Datasets/KaldiDataset.py
@@ -866,6 +866,9 @@ class ScriptTrainDataset(data.Dataset):
 
         if save_dir == '' or (not os.path.exists(os.path.join(save_dir, 'spk2idx'))):
             spk_to_idx = {speakers[i]: i for i in range(len(speakers))}
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+                
             with open(os.path.join(save_dir, 'spk2idx'), 'w') as f:
                 json.dump(spk_to_idx, f)
         else:
