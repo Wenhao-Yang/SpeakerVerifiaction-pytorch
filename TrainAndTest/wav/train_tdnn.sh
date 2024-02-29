b278e04f766a07d1613fc43781303e1a01c95061
wenhao-mac <874681044@qq.com>
2024-02-29 23:12:30 +0800

    [train] v1 add noise

--- TrainAndTest/wav/train_tdnn.sh
+++ TrainAndTest/wav/train_tdnn.sh
@@ -2,7 +2,7 @@
 
 stage=10
 waited=0
-while [ $(ps 219031 | wc -l) -eq 2 ]; do
+while [ $(ps 191286 | wc -l) -eq 2 ]; do
   sleep 60
   waited=$(expr $waited + 1)
   echo -en "\033[1;4;31m Having waited for ${waited} minutes!\033[0m\r"
@@ -112,7 +112,13 @@ if [ $stage -le 10 ]; then
 
       # CUDA_VISIBLE_DEVICES=0,5 OMP_NUM_THREADS=8 torchrun --nproc_per_node=2 --master_port=41715 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/ecapa/last_epoch/vox1_brain_aug53_noise_mag.yaml --seed=${seed}
 
-      CUDA_VISIBLE_DEVICES=0,5 OMP_NUM_THREADS=8 torchrun --nproc_per_node=2 --master_port=41715 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/ecapa/last_epoch/v1_half/vox1_brain_aug53.yaml --seed=${seed}
+      # CUDA_VISIBLE_DEVICES=0,5 OMP_NUM_THREADS=8 torchrun --nproc_per_node=2 --master_port=41715 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/ecapa/last_epoch/v1_half/vox1_brain_aug53.yaml --seed=${seed}
+
+
+      for add in 01 02 20 100 ; do
+        CUDA_VISIBLE_DEVICES=0,5 OMP_NUM_THREADS=8 torchrun --nproc_per_node=2 --master_port=41715 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/ecapa/last_epoch/v1_half/add_noise/noise_add${add}.yaml --seed=${seed}
+      done
+
 
       # CUDA_VISIBLE_DEVICES=0,5 OMP_NUM_THREADS=8 torchrun --nproc_per_node=2 --master_port=41715 TrainAndTest/train_egs/train_dist.py --train-config=TrainAndTest/wav/ecapa/last_epoch/vox1_brain_aug53_pattenoise.yaml --seed=${seed}
 
