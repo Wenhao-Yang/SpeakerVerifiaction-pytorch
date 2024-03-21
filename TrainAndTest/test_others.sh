00e67fafbcd582438539b1548c9da6c21af30652
wenhao-mac <874681044@qq.com>
2024-03-21 17:55:06 +0800

    [train] ws dist

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -3838,7 +3838,7 @@ if [ $stage -le 606 ]; then
   
   sname=dev
   for epoch in avg3 ; do #1 2 5 6 9 10 12 13 17 20 21 25 26 27 29 30 33 37 40 41
-    for model_name in wasse1 fix ;do 
+    for model_name in cosine ;do 
     common_path=ECAPA_brain/Mean_batch48_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox1/wave_fb80_inst_aug53_mix
     echo -e "\n\033[1;4;31m Stage${stage}: Test ${model_name} in ${test_set} \033[0m\n"
       for test_subset in test_radio_chn2 test_radchn2_dist1 test_radchn2_dist3; do #test_radio_chn2
@@ -3855,6 +3855,9 @@ if [ $stage -le 606 ]; then
         elif [[ $model_name == fix ]];then
           model_dir=${common_path}wasse1fix/${seed}
           yaml_name=${common_path}wasse1fix/model.yaml
+        elif [[ $model_name == fix ]];then
+          model_dir=${common_path}cosine1/${seed}
+          yaml_name=${common_path}cosine1/model.yaml
         fi
 
         xvector_dir=Data/xvector/${model_dir}/${testset}_${test_subset}_${test_input}_${epoch}
