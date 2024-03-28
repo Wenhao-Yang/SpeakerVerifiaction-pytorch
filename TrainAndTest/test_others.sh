ad5d43e81f0793e5182c5bf962188688da3ea232
wenhao-mac <874681044@qq.com>
2024-03-28 18:29:08 +0800

    [train] inbn 05

--- TrainAndTest/test_others.sh
+++ TrainAndTest/test_others.sh
@@ -3834,26 +3834,28 @@ if [ $stage -le 606 ]; then
   train_set=vox1 test_set=vox1 # #jukebox cnceleb
   train_subset=
   subset=test test_input=var test_subset=test
-  gpu_id=0
+  gpu_id=1
   
   sname=dev
-  for chn in 384 ; do
+  for chn in 256 ; do
     if [[ $chn == 384 ]];then
       chn_str=_chn384
     elif [[ $chn == 256 ]];then
       chn_str=
     fi
 
-  for epoch in avg2 ; do # avg3 1 2 5 6 9 10 12 13 17 20 21 25 26 27 29 30 33 37 40 41
-    for model_name in baseline ;do 
+  for epoch in avg3 ; do # avg2 1 2 5 6 9 10 12 13 17 20 21 25 26 27 29 30 33 37 40 41
+    for model_name in baseline inst05;do 
     # common_path=ECAPA_brain/Mean_batch48_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox1/wave_fb80_inst_aug53_mix
     # common_path=ECAPA_brain/Mean_batch48_SASP2_em192${chn_str}_official_2s/arcsoft_adam_cyclic/vox1/wave_fb80_inst_aug53_mix
     # common_path=ECAPA_brain/Mean_batch48_inbn_SASP2_em192${chn_str}_official_2s/arcsoft_adam_cyclic/vox1/wave_fb80_inst_aug53_mix
+    common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox1/wave_fb80_inst_aug53
     # common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox2/wave_fb80_inst2_aug53
-    common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox2/wave_fb80_inst2_radsnr05_aug53
+    # common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox2/wave_fb80_inst2_radsnr05_aug53
 
     echo -e "\n\033[1;4;31m Stage${stage}: Test ${model_name} in ${test_set} \033[0m\n"
-      for test_subset in all_radsnr0 all_radsnr05 all_radsnr1 all_radsnr2 all_radsnr5 ; do #test_radio_chn2 test_radchn2_dist1 test_radchn2_dist3
+      for test_subset in test test_radio_chn2 test_radchn2_dist1 test_radchn2_dist3  ; do #test_radio_chn2 test_radchn2_dist1 test_radchn2_dist3
+      # all all_radsnr0 all_radsnr05 all_radsnr1 all_radsnr2 all_radsnr5
       for seed in 1234 ; do
         if [[ $model_name == baseline ]];then
           model_dir=${common_path}/${seed}
@@ -3882,6 +3884,9 @@ if [ $stage -le 606 ]; then
         elif [[ $model_name == hidden ]];then
           model_dir=${common_path}grlwarmhidden/${seed}
           yaml_name=${common_path}grlwarmhidden/model.yaml
+        elif [[ $model_name == inst05 ]];then
+          model_dir=${common_path}_inst05/${seed}
+          yaml_name=${common_path}_inst05/model.yaml
         fi
 
         xvector_dir=Data/xvector/${model_dir}/${testset}_${test_subset}_${test_input}_${epoch}
@@ -3899,7 +3904,7 @@ if [ $stage -le 606 ]; then
             --cos-sim --test
         done
 
-        for trials in original easy hard ; do # original easy hard
+        for trials in original ; do # original easy hard
           python -W ignore TrainAndTest/train_egs/test_egs.py \
             --train-dir ${lstm_dir}/data/${train_set}/${sname} \
             --train-extract-dir ${lstm_dir}/data/${train_set}/dev \
