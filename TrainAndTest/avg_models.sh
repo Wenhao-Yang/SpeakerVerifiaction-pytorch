a2f4adc2fa4ab5cf4ab36f676cba05558fad304c
wenhao-mac <874681044@qq.com>
2024-03-30 17:10:28 +0800

    [misc] figs

--- TrainAndTest/avg_models.sh
+++ TrainAndTest/avg_models.sh
@@ -123,9 +123,9 @@ if [ $stage -le 20 ]; then
 
     # common_path=ECAPA_brain/Mean_batch48_SASP2_em192_chn384_official_2s/arcsoft_adam_cyclic/vox1/wave_fb80_inst_aug53_mixgrl
     # common_path=ECAPA_brain/Mean_batch48_inbn_SASP2_em192_chn384_official_2s/arcsoft_adam_cyclic/vox1/wave_fb80_inst_aug53_mix
-    # common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox2/wave_fb80_inst2_aug53
-    common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox2/wave_fb80_inst2_radsnr05_aug53
-    for model_name in baseline ; do
+    common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox2/wave_fb80_inst2_aug53
+    # common_path=ECAPA_brain/Mean_batch96_SASP2_em192_official_2s/arcsoft_adam_cyclic/vox2/wave_fb80_inst2_radsnr05_aug53
+    for model_name in inbn05 ; do
     echo -e "\n\033[1;4;31m Stage${stage}: Average model: ${model_name} \033[0m\n"
         for seed in 1234 ; do
             if [[ $model_name == baseline ]];then
@@ -140,6 +140,8 @@ if [ $stage -le 20 ]; then
                 model_dir=${common_path}wasse1fix/${seed}
             elif [[ $model_name == cosine ]];then
                 model_dir=${common_path}cosine1/${seed}
+            elif [[ $model_name == inbn05 ]];then
+                model_dir=${common_path}_inbn05/${seed}
             fi
             
             python -W ignore TrainAndTest/train_egs/average_model.py \
